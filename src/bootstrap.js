0076620db10b5f3d55601ced4addc2c370fb5c7c
hiroshi <hiroshi@wovn.io>
2020-12-18 17:56:27 +0900

    Add basci tests

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,6 +2,12 @@
 function str(obj) {
   return JSON.stringify(obj, null, 2)
 }
+function put(s, ...args) {
+  process.stdout.write(s)
+  for (const arg of args) {
+    console.log('', arg)
+  }
+}
 function puts(...args) {
   for (const arg of args) {
     console.dir(arg, {depth: null})
@@ -17,7 +23,7 @@ function tokenize(src) {
     match(p, 'num', /^[0-9]+(\.[0-9]+)?/) ||
     match(p, 'id', /^[A-Za-z_][A-Za-z0-9_]*(,[A-Za-z_][A-Za-z0-9_]*)*/) ||
     match(p, 'str', /^"(?:(?:\\")|[^"])*"/) ||
-    match(p, 'prop', /^.[A-Za-z_][A-Za-z0-9_]*/) ||
+    match(p, 'prop', /^\.[A-Za-z_][A-Za-z0-9_]*/) ||
     match(p, 'spaces', /^[ #\n]+/) ||
     some(p, 'la', '[') ||
     some(p, 'ra', ']') ||
@@ -25,35 +31,32 @@ function tokenize(src) {
     some(p, 'rp', ')') ||
     some(p, 'op2', '+= -= *= /= || && == != >= <= ++ => := = : <- > < + - * /')
 
+  let indent = 0
   let pos=0, tokens=[]
   while (pos < src.length) {
     const token = eat(pos)
     if (!token) { throw new Error('tokenize at ' + pos) }
+    if (token.tag === 'spaces' && token.code.includes('\n')) {
+      indent = token.code.split('\n').slice(-1)[0].length
+      if (indent % 2 != 0) { throw new Error('invalid indent=' + indent + ' at ' + token.pos) }
+    }
+    token.indent = indent
     pos += token.code.length
     tokens.push(token)
   }
 
   const dst = tokens.map(t => t.code).join('')
   if (src !== dst) throw new Error('tokenize assertion: src=' + str(src) + ' dst=' + str(dst))
-  let indent = 0
-  for (const token of tokens) {
-    if (token.tag === 'spaces') {
-      indent = token.code.split('\n').slice(-1)[0].length
-      if (indent % 2 != 0) { throw new Error('invalid indent=' + indent + ' at ' + token.pos) }
-    } else {
-      token.indent = indent
-    }
-  }
   return tokens.filter(t => t.tag !== 'spaces')
 }
 function parse(tokens) {
   const nodes = []
   let pos = 0
   function info() {
-    return ' at=' + pos + ' tokens=' + str(tokens) + ' nodes=' + nodes
+    return ' at=' + pos + ' tokens=' + str(tokens) + ' nodes=' + str(nodes)
   }
-  function look() {
-    return (tokens[pos+1] || {tag:'EOT'}).tag
+  function nextTag() {
+    return (tokens[pos] || {tag: 'EOT'}).tag
   }
   function until(end) {
     const ary = []
@@ -63,60 +66,37 @@ function parse(tokens) {
     }
     return ary
   }
-  function setArgv(token) {
-    token.argv = []
-    const next = look()
-    if (next.code === '(' && next.pos === token.pos + token.length) {
-      eat()
-      token.argv = until(')')
-    }
+  function eatArgv(token) {
+    return nextTag() === 'lp' ? until(')') : []
   }
   function eat() {
     const token = tokens[pos++]
-    switch (look()) {
-      case 'EOT':
-      case 'num':
-      case 'str':
-      case 'ra':
-      case 'rp':
-        return token
-
-      case 'id':
-        setArgv(token)
-        return token
-
+    switch (nextTag()) {
       case 'op2':
         const op2 = eat()
-        if (op2.code !== 'op2') { throw new Error('Bug '+ str(op2)) }
-        op2.lhs =token
+        op2.lhs = token
         op2.rhs = eat()
         return op2
 
       case 'prop':
         const prop = eat()
         prop.target = nodes[node.length - 1]
-        setArgv(prop)
+        prop.argv = eatArgv()
         return prop
 
-      case 'la':
-        token.ary = until(']')
-        return token
-
-      case 'lp':
-        token.exp = eat()
-        if (eat().code !== ')') { throw new Error('Invalid end of parentheses' + info()) }
-        return token
-
-        if (token.code === '(') {
-        } else if (token.code === ']') {
-          token.ary = until(']')
-          return token
-        } else {
-          throw new Error('Bug ' + str(token))
-        }
-
-      default:
-          throw new Error('Bug ' + str(look()))
+     default:
+       switch (token.tag) {
+         case 'num': return token
+         case 'str': return token
+         case 'ra': return token
+         case 'rp': return token
+         case 'op2': return token
+         case 'id': token.argv = eatArgv(); return token
+         case 'la': token.ary = until(']'); return token
+         case 'lp': token.items = until(')'); return token
+         default:
+           throw new Error('Unexpected tag ' + str(token) + info())
+       }
     }
   }
   while (pos < tokens.length) {
@@ -126,15 +106,78 @@ function parse(tokens) {
   }
   return nodes
 }
-function compile(token) {
-  puts(token)
-  return token
+function generate(token) {
+  function genCall(argv) {
+    return argv.length === 0 ? '' : '(' + argv.map(generate).join(',') + ')'
+  }
+  switch (token.tag) {
+    case 'id':
+    case 'num':
+    case 'str': return token.code
+    case 'la': return '[' + token.ary.map(generate).join(',') + ']'
+    case 'lp': return '(' + token.items.map(generate).join(',') + ')'
+    case 'op2': return generate(token.lhs) + token.code + generate(token.rhs)
+    case 'prop': return generate(token.target) + genCall(prop.argv)
+    default:
+      throw new Error('Bug ' + str(token))
+  }
 }
-function main() {
+function compile(src) {
+  const tokens = tokenize(src)
+  const defs = parse(tokens)
+  const js = defs.map(generate).join('\n')
+  return js
+}
+function evalInSandbox(js) {
+  try {
+    return Function(js)()
+  } catch (e) {
+    puts('Failed to evaluate')
+    for (const [i, line] of js.split('\n').entries()) {
+      put((1 + i).toString().padStart(3) + ':', line)
+    }
+    return e
+  }
+}
+function testAll() {
+  function eq(expect, main) {
+    const src = 'main = ' + main
+    const js = compile(src) + '\nreturn main.valueOf()'
+    const actual = evalInSandbox(js)
+    if (str(expect) === str(actual)) {
+      put('.')
+    } else {
+      console.error('Failed')
+      put('expect: ', expect)
+      put('actual: ', actual)
+      put('js: ', js)
+      process.exit(1)
+    }
+  }
+
+  // basic values
+  eq(1, '1')
+  eq(true, 'true')
+  eq('a', '"a"')
+  eq([], '[]')
+  eq([1, 2, 3], '[1 2 3]')
+
+  // check parenthese handling
+  eq(1, '(1)')
+
+  // check spaces handling
+  eq(1, ' 1 ')
+
+  puts('ok')
+}
+function compileStdin() {
   const src = require('fs').readFileSync('/dev/stdin', 'utf8')
   const tokens = tokenize(src)
   const defs = parse(tokens)
   const js = defs.map(compile).join('\n')
   console.log(js)
+}
+function main() {
+  process.argv[2] === 'test' ? testAll() : compileStdin()
 }
 main()
