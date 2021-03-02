c9eeb70e64644aa939fadd4897624073078393c6
hiroshi <hiroshi@wovn.io>
2021-03-02 22:25:54 +0900

    implement values for string, array

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,20 +1,23 @@
 'use strict'
 const print = (...args) => console.log(...args)
-const str = s => JSON.stringify(s)
+const dump = s => JSON.stringify(s)
 function tokenize(src) {
   const len = src.length
   let pos = 0
   let indent = 0
   const newToken = (tag,code) => ({tag,code,pos,indent})
   const find = (tag, m) => m ? newToken(tag, m[0]) : null
+  const any = (tag, s, ary) => find(tag, ary.find(a => s.slice(0, a.length) === a))
   const rule = s =>
     find('id', s.match(/^[A-Za-z_][A-Za-z_0-9]*/)) ||
     find('num', s.match(/^[0-9]+/)) ||
-    find('sym', s.match(/^[+\-*/=]+/)) ||
+    find('str', s.match(/^"[^"]*"/)) ||
+    any('sym', s, '+ - * / = [ ]'.split(' ')) ||
     find('spaces', s.match(/^[ \t\r\n]+/))
   const tokens = []
   while (pos < len) {
     const token = rule(src.slice(pos))
+    if (!token) { throw new Error('Failed to tokenize: ' + src.slice(pos)) }
     pos += token.code.length
     tokens.push(token)
   }
@@ -28,21 +31,30 @@ function parse(tokens) {
     if (pos >= len) { throw new Error('Out of index: ' + pos) }
     const token = tokens[pos]
     pos++
-    if (tag && token.tag !== tag) { throw new Error('Unexpected tag: ' + str(tag)) }
+    if (tag && token.tag !== tag) { throw new Error('Unexpected tag: ' + dump(tag)) }
     return token
+  }
+  function consumes(f) {
+    const tokens_ = []
+    while (pos < len && f(tokens[pos])) {
+      tokens_.push(parse_unit())
+    }
+    return tokens_
   }
   function until(f) {
     const units = []
-    while (pos < len && f(tokens[pos])) {
-      units.push(parse_unit())
+    while (pos < len) {
+      const unit = parse_unit()
+      units.push(unit)
+      if (!f(unit)) { break }
     }
     return units
   }
   function parse_function() {
     const fname = consume('id')
-    const args = until(t => t.tag === 'id')
+    const args = consumes(t => t.tag === 'id')
     const sym = consume('sym')
-    if (sym.code !== '=') { throw new Error('define function should contains =: ' + str(eq)) }
+    if (sym.code !== '=') { throw new Error('define function should contains =: ' + dump(eq)) }
     sym.fname = fname
     sym.args = args
     sym.body = parse_unit()
@@ -51,9 +63,15 @@ function parse(tokens) {
   function parse_unit() {
     const token = consume()
     switch (token.tag) {
-      case 'sym': return token
-      case 'num': return token
-      default: throw new Error('Unexpected token: ' + str(token))
+      case 'num':
+      case 'str': return token
+      case 'sym':
+          switch (token.code) {
+            case '[':
+              token.list = until(u => u.code !== ']').slice(0, -1)
+            default: return token
+          }
+      default: throw new Error('Unexpected token: ' + dump(token))
     }
   }
   const defines = []
@@ -68,13 +86,15 @@ function generate(nodes) {
   }
   function gen(token) {
     switch (token.tag) {
+      case 'num':
+      case 'str': return token.code
       case 'sym':
         switch (token.code) {
           case '=': return genFunc(token.fname.code, token.args, token.body)
-          default: throw Error('Gen sym error ' + str(token))
+          case '[': return '[' + token.list.map(gen).join(', ') + ']'
+          default: throw Error('Gen sym error ' + dump(token))
         }
-      case 'num': return token.code
-      default: throw Error('Gen error ' + str(token))
+      default: throw Error('Gen error ' + dump(token))
     }
   }
   return nodes.map(gen).join('\n')
@@ -97,7 +117,7 @@ function testAll() {
     const src = 'main = ' + exp
     const result = run(src)
     const actual = result.actual
-    if (expect === actual) {
+    if (dump(expect) === dump(actual)) {
       process.stdout.write('.')
     } else {
       print('Failed: ', src)
@@ -112,6 +132,11 @@ function testAll() {
 
   // value
   eq(1, '1')
+  eq('hi', '"hi"')
+  eq([], '[]')
+  eq([1], '[1]')
+  eq([1, 2], '[1 2]')
+  eq(['a', 'b'], '["a" "b"]')
 
   // exp
 
