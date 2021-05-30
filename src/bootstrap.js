bf3e2246769ad8ea4017e472e150689d7e30b666
hiroshi <hiroshi@wovn.io>
2021-05-30 16:50:31 +0900

    Add evaluator

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -32,11 +32,11 @@ function tokenize(src) {
       line,
       index: s.index,
     }
-    if (token[0].match(/^[a-zA-Z_]/)) { o.is_id = true }
+    if (token[0].match(/^[a-zA-Z_]/)) { o.id = o.token }
     else if (token[0].match(/^[0-9.]+$/)) { o.value = parseInt(o.token) }
     else if (token[0].match(/^["'`]/)) { o.value = o.token.slice(1, -1) }
-    else if (token[0].match(/^[=:|]$/)) { o.is_sym = true }
-    else { o.is_op = true }
+    else if (token[0].match(/^[=:|]$/)) { o.sym = o.token }
+    else { o.op2 = o.token }
     return o
   }).filter(x => x.token.trim())
 }
@@ -52,7 +52,7 @@ function parse(tokens) {
     while (true) {
       const next = consume()
       if (!next) { return ret }
-      if (next.is_op) {
+      if (next.op2) {
         const rhs = unit()
         if (rhs.op2) {
           const o1 = next.token
@@ -78,12 +78,12 @@ function parse(tokens) {
     return a
   }
   function parse_define() {
-    const [name, ...args] = until(x => x.is_id, consume).map(t => t.token)
+    const [name, ...args] = until(x => x.id, consume).map(t => t.token)
     const sym = consume()
     switch (sym.token) {
-      case '=': return {name, args, body: parse_body(), is_func: true}
-      case ':': return {name, args, body: parse_struct(), is_struct: true}
-      case '|': return {name, args, body: parse_enum(), is_enum: true}
+      case '=': return {name, args, body: parse_body()}
+      case ':': return {name, args, struct: parse_struct()}
+      case '|': return {name, args, enum: parse_enum()}
       default: throw err('Unknown token', {sym,pos,tokens})
     }
   }
@@ -102,16 +102,30 @@ function parse(tokens) {
   return nodes
 }
 function evaluate(nodes) {
-  const env = {}
+  const environment = function(parent) {
+    this.get = key => this['__' + key] || parent + ['__' + key] || (() => { throw err('not found', {key,dit,parent}) })()
+    this.put = (key,value) => this['__' + key] = value
+  }
+  const root = new environment({})
   for (const node of nodes) {
-    env[node.name.token] = node
+    root.put(node.name, node)
   }
-  function run(node) {
-    if (node.is_func) { return run(node.body) }
+  function run(node, env) {
     if (node.value) { return node.value }
-    throw err('Unknown node', {node})
+    if (node.body) { return run(node.body, env) }
+    if (node.id) { return run(env.get(node.id), env) }
+    if (node.op2) {
+      switch (node.op2) {
+        case '+': return run(node.lhs, env) + run(node.rhs, env)
+        case '-': return run(node.lhs, env) - run(node.rhs, env)
+        case '*': return run(node.lhs, env) * run(node.rhs, env)
+        case '/': return run(node.lhs, env) / run(node.rhs, env)
+        default: throw err('Unknown operator', node)
+      }
+    }
+    throw err('Unknown node', {node, env})
   }
-  return run(env.main)
+  return run(root.get('main').body, root)
 }
 function run(src) {
   const tokens = tokenize(src)
@@ -126,33 +140,33 @@ function testTokenize() {
     test(expect, tokenize(src), {src})
   }
   t([
-    { token: 'a', indent: 0, line: 1, index: 0, is_id: true },
-    { token: '=', indent: 0, line: 1, index: 2, is_sym: true },
+    { token: 'a', indent: 0, line: 1, index: 0, id: 'a' },
+    { token: '=', indent: 0, line: 1, index: 2, sym: '=' },
     { token: '1', indent: 0, line: 1, index: 4, value: 1 }
   ], 'a = 1')
   t([
-    { token: 'a', indent: 0, line: 1, index: 0, is_id: true },
-    { token: '=', indent: 0, line: 1, index: 2, is_sym: true },
+    { token: 'a', indent: 0, line: 1, index: 0, id: 'a' },
+    { token: '=', indent: 0, line: 1, index: 2, sym: '=' },
     { token: '"a"', indent: 0, line: 1, index: 4, value: 'a' }
   ], 'a = "a"')
   t([
-    { token: 'struct', indent: 0, line: 1, index: 0, is_id: true },
-    { token: 'a', indent: 0, line: 1, index: 7, is_id: true },
-    { token: ':', indent: 0, line: 1, index: 8, is_sym: true },
-    { token: 'value', indent: 2, line: 2, index: 12, is_id: true },
-    { token: 'a', indent: 2, line: 2, index: 18, is_id: true },
-    { token: 'method', indent: 2, line: 3, index: 22, is_id: true },
-    { token: 'offset', indent: 2, line: 3, index: 29, is_id: true },
-    { token: '=', indent: 2, line: 3, index: 36, is_sym: true },
-    { token: 'value', indent: 2, line: 3, index: 38, is_id: true },
-    { token: '+', indent: 2, line: 3, index: 44, is_op: true },
-    { token: 'offset', indent: 2, line: 3, index: 46, is_id: true }
+    { token: 'struct', indent: 0, line: 1, index: 0, id: 'struct' },
+    { token: 'a', indent: 0, line: 1, index: 7, id: 'a' },
+    { token: ':', indent: 0, line: 1, index: 8, sym: ':' },
+    { token: 'value', indent: 2, line: 2, index: 12, id: 'value' },
+    { token: 'a', indent: 2, line: 2, index: 18, id: 'a' },
+    { token: 'method', indent: 2, line: 3, index: 22, id: 'method' },
+    { token: 'offset', indent: 2, line: 3, index: 29, id: 'offset' },
+    { token: '=', indent: 2, line: 3, index: 36, sym: '=' },
+    { token: 'value', indent: 2, line: 3, index: 38, id: 'value' },
+    { token: '+', indent: 2, line: 3, index: 44, op2: '+' },
+    { token: 'offset', indent: 2, line: 3, index: 46, id: 'offset' }
   ], 'struct a:\n  value a\n  method offset = value + offset')
 }
 function testParse() {
   function show(node) {
     return node.value ? node.value.toString() :
-      node.is_id ? node.token :
+      node.id ? node.id :
       node.body ? node.name + node.args.map(x => ' ' + x).join('') + ' = ' + show(node.body) :
       node.call ? node.name + '(' + node.args.map(show).join(' ') + ')' :
       node.op2 ? '(' + show(node.lhs) + ' ' + node.op2 + ' ' + show(node.rhs) + ')' :
@@ -172,17 +186,21 @@ function testEvaluate() {
   function t(expect, exp, ...defs) {
     defs.push('main = ' + exp)
     const src = defs.join('\n')
-    test(expect, src, run(src).value)
+    test(expect, run(src).value, src)
   }
   t(1, '1')
   t('hi', '"hi"')
+  t(7, '1+2*3')
+  t(5, '1*2+3')
+  t(1, 'f', 'f=1')
+  //t(1, 'f(1)', 'f a = a + 1')
 }
 
 // main
 function main() {
   testTokenize()
   testParse()
-  //testEvaluate()
+  testEvaluate()
   puts('ok')
 }
 main()
