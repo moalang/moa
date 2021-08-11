62700b6de50ad237d4f19856d2fd6ff50ee5e769
hiroshi <hiroshi@wovn.io>
2021-08-12 00:37:56 +0900

    Support recursive function calling

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -8,21 +8,23 @@ const dict = (ks,vs) => ks.reduce((d,k,i) => (d[k]=vs[i], d), {})
 
 const priorities = [
   '|| &&',
-  '== !=',
+  '== != > < >= <=',
   '+ -',
   '* / //',
+  '.',
 ].map(ops => ops.split(' '))
 const priority = op => priorities.findIndex(ops => ops.includes(op))
 
 const tokenize = src => {
   let index = 0
-  const match = (tag, reg, f) => (m => m && ({tag, index, code: m[0], value: f && f(m[0])}))(src.slice(index).match(reg))
-  const any = (tag, a) => (code => code && ({tag, index, code}))(a.find(v => src.slice(index).startsWith(v)))
+  let line = 1
+  const match = (tag, reg, f) => (m => m && ({tag, index, line, code: m[0], value: f && f(m[0])}))(src.slice(index).match(reg))
+  const any = (tag, a) => (code => code && ({tag, index, line, code}))(a.find(v => src.slice(index).startsWith(v)))
   const next = () => match('int', /^[0-9]+/, parseInt) ||
     match('string', /^"[^"]*"+/, s => s.slice(1, -1)) ||
     match('id', /^(?:true|false)(?![a-zA-Z0-9_])/, s => s === 'true') ||
     match('id', /^[a-zA-Z_0-9]+/) ||
-    any('op2', '|| && == != //'.split(' ').concat('+-*/'.split(''))) ||
+    any('op2', '|| && == >= <= != //'.split(' ').concat('+-*/.><'.split(''))) ||
     any('sym', '[]()='.split('')) ||
     match('space', /^[ \n]+/) ||
     fail('Failed to tokenize:', {src, index, around: src.slice(index)})
@@ -30,6 +32,7 @@ const tokenize = src => {
   while (index < src.length) {
     const token = next()
     index += token.code.length
+    line += (token.code.match(/\n/g) || []).length
     tokens.push(token)
   }
   return tokens.filter(t => t.tag !== 'space')
@@ -40,13 +43,22 @@ const parse = tokens => {
   const next = () => tokens[pos++] || fail('EOT', tokens)
   const consume = () => {
     let node = next()
-    if (node.code === ')') {
+    if (node.code === ')' || node.code === ']') {
       return node
     }
     if (node.code === '(') {
       node = consume()
       guard(t => t.code === ')')
       node.fixed = true
+    }
+    if (node.code === '[') {
+      const values = until(t => t.code !== ']', consume)
+      node = newType('array', {values})
+    }
+    while (pos < tokens.length && tokens[pos].code === '(' && tokens[pos - 1].index + tokens[pos - 1].code.length === tokens[pos].index) {
+      pos += 1 // drop "("
+      const argv = until(t => t.code !== ')', consume)
+      node = newType('call', {body: node, argv})
     }
     if (pos < tokens.length && tokens[pos].tag === 'op2') {
       const op2 = consume().code
@@ -58,14 +70,9 @@ const parse = tokens => {
         node = {op2, lhs, rhs}
       }
     }
-    if (pos < tokens.length && tokens[pos].code === '(' && tokens[pos - 1].index + tokens[pos - 1].code.length === tokens[pos].index) {
-      pos += 1 // drop "("
-      const argv = until(t => t.code !== ')', consume)
-      node = newType('call', {argv, body: node})
-    }
     return node
   }
-  const guard = f => (node => f(node) ? node : fail('Unexpected node:' + f.toString(), node))(consume())
+  const guard = f => (node => f(node) ? node : fail('Unexpected node:' + f.toString(), {node, pos, nodes}))(consume())
   const until = (f, g) => {
     const matches = []
     while (pos < tokens.length) {
@@ -81,20 +88,12 @@ const parse = tokens => {
     g && g()
     return matches
   }
-  const exp = () => {
-    const node = consume()
-    if (node.code === '[') {
-      const values = until(t => t.code !== ']', consume)
-      return newType('array', {values})
-    }
-    return node
-  }
   const top = () => {
-    const id = consume().code
-    const args = until(t => t.tag === 'id').map(t => t.code)
-    guard(t => t.code === '=')
-    const body = exp()
-    return newType('func', {id, args, body})
+    const {code, indent} = consume()
+    const args = until(t => t.tag === 'id' && t.indent === indent).map(t => t.code)
+    guard(t => t.code === '=' && t.indent === indent)
+    const body = consume()
+    return newType('func', {id: code, args, body})
   }
 
   const nodes = []
@@ -110,58 +109,96 @@ const execute = nodes => {
       scope[node.id] = node
     }
   }
-  const run = node => {
-    if (node.value !== undefined) {
-      return node.value
-    }
-    if (node.values) {
-      return node.value = node.values.map(run)
-    }
-    if (node.op2) {
-      switch (node.op2) {
-        case '+': return node.value = run(node.lhs) + run(node.rhs)
-        case '-': return node.value = run(node.lhs) - run(node.rhs)
-        case '*': return node.value = run(node.lhs) * run(node.rhs)
-        case '/': return node.value = run(node.lhs) / run(node.rhs)
-        case '//': return node.value = parseInt(run(node.lhs) / run(node.rhs))
-        case '==': return node.value = eq(run(node.lhs), run(node.rhs))
-        case '!=': return node.value = !eq(run(node.lhs), run(node.rhs))
-        case '||': return node.value = run(node.lhs) || run(node.rhs)
-        case '&&': return node.value = run(node.lhs) && run(node.rhs)
-        default: fail('Unknown op2', node)
+  const method = (env, o, node) => {
+    if (node.tag === 'id') {
+      const id = node.code
+      if (typeof o === 'object' && o.constructor === Array) {
+        if (id === 'size') {
+          return o.length
+        }
+      }
+      if (typeof o === 'string') {
+        if (id === 'size') {
+          return o.length
+        }
+      }
+    } else if (node.type === 'call') {
+      const id = node.body.code
+      const argv = node.argv
+      if (typeof o === 'object' && o.constructor === Array) {
+        if (id === 'at') {
+          return o[run(env, argv[0])]
+        }
+      }
+      if (typeof o === 'string') {
+        if (id === 'at') {
+          return o[run(env, argv[0])]
+        }
       }
     }
-    if (node.type === 'func') {
+    fail('Unknown method', {o,node})
+  }
+  const run = (env, node) => {
+    if (node === undefined) {
+      return node
+    }
+    const t = typeof node
+    if (t === 'number' || t === 'string' || (t === 'object' && node.constructor === Array)) {
+      return node
+    }
+    if (node.value !== undefined) {
+      return node.value
+    } else if (node.values) {
+      return node.values.map(o => run(env, o))
+    } else if (node.op2 === '.') {
+      return method(env, run(env, node.lhs), node.rhs)
+    } else if (node.op2) {
+      const l = run(env, node.lhs)
+      const r = run(env, node.rhs)
+      switch (node.op2) {
+        case '+': return l + r
+        case '-': return l - r
+        case '*': return l * r
+        case '/': return l / r
+        case '//': return parseInt(l / r)
+        case '==': return eq(l, r)
+        case '!=': return !eq(l, r)
+        case '>':  return l > r
+        case '<':  return l < r
+        case '>=': return l >= r
+        case '<=': return l <= r
+        case '||': return l || r
+        case '&&': return l && r
+        default: fail('Unknown op2', node)
+      }
+    } else if (node.type === 'func') {
       if (node.args.length === 0) {
-        return node.value = run(node.body)
+        return run(env, node.body)
       } else {
         return node
       }
-    }
-    if (node.type === 'call') {
+    } else if (node.type === 'call') {
       if (node.body.code === 'if') {
         for (let i=0; i<node.argv.length; i+=2) {
-          if (run(node.argv[i]) === true) {
-            return run(node.argv[i + 1])
+          if (run(env, node.argv[i]) === true) {
+            return run(env, node.argv[i + 1])
           }
         }
-        return run(node.argv.slice(-1)[0])
+        return run(env, node.argv.slice(-1)[0])
       }
-      const f = run(scope[node.body.code])
+      const f = run(env, env[node.body.code])
       if (f.type === 'func') {
-        const d = dict(f.args, node.argv)
-        Object.assign(scope, d) // TODO: to use local scope
-        return run(f.body)
+        const argv = node.argv.map(o => run(env, o))
+        return run(Object.assign({}, env, dict(f.args, argv)), f.body)
       } else {
         return f
       }
-    }
-    if (node.tag === 'id') {
-      return run(scope[node.code])
+    } else if (node.tag === 'id') {
+      return run(env, env[node.code])
     }
     fail('Failed to run', {node, nodes})
   }
-  return run(scope.main.body)
+  return run(scope, scope.main.body)
 }
 
 const testJs = () => {
@@ -195,6 +232,8 @@ const testJs = () => {
   t(2, '4 // 2')
   t(true, '1 != 2')
   t(false, '1 == 2')
+  t(false, '1 > 2')
+  t(true, '1 < 2')
 
   // parenthesis
   t(3, '(1 + 2)')
@@ -219,18 +258,19 @@ const testJs = () => {
   // functions
   t(3, 'f + 2', 'f = 1')
   t(3, 'f(1)', 'f a = a + 2')
-/*
-  // functions
-  t(3, 'f = 1\nmain = f + 2')
-  t(3, 'f a = a + 2\nmain = f 1')
 
   // methods
-  t(-1, 'main = 1.neg')
-  t(1, 'main = [1].size')
+  t(1, '[1].size')
+  t(2, '"hi".size')
+  t(2, 'id("hi").size', 'id a = a')
+  t('i', '"hi".at(1)')
 
   // generics
-  t('i', 'id a = a\nmain = id("hi").at(id(1))')
+  t('i', 'id("hi").at(id(1))', 'id a = a')
 
+  // recursive
+  t(120, 'f(5)', 'f n = if((n > 1) (n * f(n - 1)) 1)')
+/*
   // recursive
   t(120, 'f n = if (n > 1) (n * (f n - 1)) 1\nmain = f(5)')
 
