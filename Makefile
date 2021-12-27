ee537d1c28eb901e9454f061ab9f9716f9fe0d65
hiroshi <hiroshi@wovn.io>
2021-12-27 10:50:12 +0900

    WIP: type inference

--- Makefile
+++ Makefile
@@ -4,5 +4,5 @@ test:
 	@node bin/moa build src/moa.moa
 
 watch:
-	@make test
+	@-make test
 	@-fswatch -0 -o src/bootstrap.js src/moa.moa | xargs -I {} -n1 -0 make test
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,8 +1,19 @@
 'use strict'
 
+const write = (...a) => process.stdout.write(a.map(x => x.toString()).join(' '))
 const puts = (...a) => console.log(...a)
-const trace = (...a) => (puts('TRACE: ', ...a), a[a.length - 1])
-const fail = (m, ...a) => { a.length && trace(a); throw new Error(m) }
+const trace = (...a) => (write('TRACE:'), console.dir(a, {depth: null}), a[a.length - 1])
+const fail = (m, ...a) => { a.length && trace(m, ...a); throw new Error(m) }
+const showType = target => {
+  const show = t => t.instance ? show(t.instance) : (
+    t.name && t.types.length >= 1 ? `(${t.name} ${t.types.map(show).join(' ')})` :
+    t.name ? t.name : `(${t.types.map(show).join(' ')})`
+  ) + (t.tvars.length === 0 ? '' : `[${t.tvars.map(show).join(' ')}]`)
+  const s = show(target)
+  const o = {}
+  const r = s.replace(/\d+/g, t => o[t] ||= Object.keys(o).length + 1)
+  return r
+}
 
 Object.defineProperty(String.prototype, 'size', { get() { return this.length } });
 String.prototype.at = function (n) { return n < this.length ? this[n]  : fail('Out of index') }
@@ -14,37 +25,45 @@ Object.defineProperty(Array.prototype, 'size', { get() { return this.length } })
 Array.prototype.at = function (n) { return n < this.length ? this[n]  : fail('Out of index') }
 Array.prototype.append = function (a) { return this.concat(a) }
 Array.prototype.contains = function (s) { return this.includes(s) }
+Array.prototype.keep = function (f) { return this.filter(f) }
 
-function compile_to_js(src) {
-  const initialType = s => '"`'.includes(s[0]) ? 'string' : '0123456789'.includes(s[0]) ? 'int' : null
-  const newToken = s => Object.assign(new String(s), {type: initialType(s)})
-  const tokens = src.split(/([():\[\]]|[\+\-\*\/%&|=><\.]+|"[^"]*?"|`[^`]*?`|[ \n]+|[a-zA-Z0-9_,]+(?:\(\)|\(?))/).map(t => t.replace(/^ +/, '')).filter(x => x).map(newToken)
-  const is_op2 = x => x.match && x.match(/[\+\-\*\/%&|=><\.]/)
+function compile(src) {
+  let pos = 0
+  const newToken = s => Object.assign(new String(s.replace(/^ +/, '')), {pos: (pos+=s.length) - s.length})
+  const tokens = src.split(/(\(\)|[():\[\],]|[\+\-\*\/%&|=><\.]+|"[^"]*?"|`[^`]*?`|[ \n]+|[a-zA-Z0-9_]+)/).map(newToken).filter(x => x.length)
+  if (pos !== src.length) { fail(`Failed to tokenize ${pos} !== ${src.length}`) }
+  const is_op2 = x => x.match && x.match(/[\+\-\*\/%&|=><\.,]/)
   const parse = () => {
     let pos = 0
     const next = o => { ++pos; return o }
-    const until = f => {
+    const until = (f, g) => {
       const a = []
       while (pos < tokens.length && tokens[pos][0] != '\n' && f(tokens[pos])) {
-        a.push(consume())
+        a.push((g || consume)())
       }
       return a
     }
     const reads = f => until(f || (() => true))
     const sepby = f => [reads()].concat(pos < tokens.length && f(tokens[pos]) ? sepby(next(f)) : [])
     const consume = () => {
-      const priorities = '|| && == != > >= < <= * / % + - .'.split(' ')
-      const priority = op => priorities.findIndex(x => x == op)
-      const op2 = (op, l, r) => Array.isArray(r) && is_op2(r[0]) && priority(op) > priority(r[0]) ? [r[0], [op, l, r[1]], r[2]] : [op, l, r]
       const t = tokens[pos++]
+      const unnest = a => Array.isArray(a) && a.length === 1 ? unnest(a[0]) : a
+      const array = a => Array.isArray(a) ? a : [a]
       const node =
-        t == '(' ? next(reads(t => t != ')')) :
-        t.endsWith('(') ? [newToken(t.slice(0, -1))].concat(next(reads(t => t != ')'))) :
+        t == '(' ? unnest(next(reads(t => t != ')'))) :
         t == '[' ? [newToken('array')].concat(next(reads(t => t != ']'))) :
         t == ':' && tokens[pos][0] == '\n' ? [newToken('do')].concat(top(tokens[pos++])) :
-        t == ':' ? [newToken('do')].concat([reads()]) :
+        t == ':' ? reads() :
         t
-      return (tokens[pos] || '').match(/^[\+\-\*\/%&|=><\.]/) ? op2(tokens[pos++], node, consume()) : node
+      const combine = node => (t => !t ? node :
+        t == ',' ? combine([node].concat(until(t => t == ',' && ++pos, () => tokens[pos++]))) :
+        t == '.' ? combine([t, node, next(tokens[++pos])]) :
+        t == '=>' ? combine([t, array(node), (++pos, consume())]) :
+        t.match(/^[\+\-\*\/%&|=><]/) ? combine([tokens[pos++], node, consume()]) :
+        t == '()' ? combine((node.suffix = '()', pos++, node)) :
+        t == '(' && t.pos === tokens[pos - 1].pos +tokens[pos - 1].length ? ++pos && combine([node].concat(next(reads(t => t != ')')))) :
+        node)(tokens[pos])
+      return combine(node)
     }
     const top = br => sepby(t => t.toString() == br)
     return top('\n')
@@ -73,9 +92,153 @@ function compile_to_js(src) {
         head == '-' && tail.length === 1 ? '-' + gen(tail[0]) :
         !Array.isArray(head) && is_op2(head) ? gen(tail[0]) + head + gen(tail[1]) :
         gen(head) + '(' + tail.map(gen).join(', ') + ')'
-    return Array.isArray(node) ? apply(node) : node
+    return (Array.isArray(node) ? apply(node) : node) + (node.suffix || '')
   }
-  return parse().map(gen).join('\n')
+  const infer = nodes => {
+    const fresh = (type, nonGeneric) => {
+      const d = {}
+      const rec = t => {
+        const p = prune(t)
+        return p.var ?
+          (nonGeneric.includes(p.name) ? p : d[p.name] ||= tvar()) :
+          Object.assign({}, p, {types: p.types.map(rec)})
+      }
+      return rec(type)
+    }
+    const unify = (a, b) => {
+      a = prune(a)
+      b = prune(b)
+      if (a.var) {
+        if (a.name !== b.name) {
+          a.instance = b
+        }
+      } else if (b.var) {
+        unify(b, a)
+      } else {
+        if (a.name !== b.name || a.types.length !== b.types.length) {
+          fail(`type miss match`, showType(a), showType(b))
+        }
+        a.types.map((t,i) => unify(t, b.types[i]))
+      }
+    }
+
+    const prune = t => (t.var && t.instance) ? t.instance = prune(t.instance) : t
+    const analyse = (node, env, nonGeneric) => node.type = prune(_analyse(node, env, nonGeneric))
+    const _analyse = (node, env, nonGeneric) => {
+      if (Array.isArray(node)) {
+        let [head, ...tail] = node
+        if (head == '=>') {
+          const argv = tail[0].map(_ => tvar())
+          const newEnv = Object.assign({}, env)
+          tail[0].map((arg, i) => arg.type = newEnv[arg.toString()] = argv[i])
+          return tlambda(...argv, analyse(tail[1], newEnv, nonGeneric.concat(argv.map(t => t.name))))
+        } else if (head == 'var' || head == 'let' || head == '=') {
+          const name = tail[0].toString()
+          return head.type = env[name] = analyse(tail.slice(1), env, nonGeneric)
+        } else if (head == 'def') {
+          const name = tail[0].toString()
+          const args = tail.slice(1, -1).map(arg => (arg.type = tvar(), arg))
+          const body = tail.slice(-1)[0]
+          const newEnv = Object.assign({}, env)
+          args.forEach(arg => newEnv[arg.toString()] = arg.type)
+          const ret = analyse(body, newEnv, nonGeneric.concat(args.map(t => t.type.name)))
+          return head.type = env[name] = tlambda(...args.map(t => t.type), ret)
+        } else if (head == 'do') {
+          const steps = tail.map(step => analyse(step, env, nonGeneric))
+          return head.type = steps[steps.length - 1]
+        } else if (head == 'array') {
+          const t = tarray(tvar())
+          tail.map(x => unify(t.tvars[0], analyse(x, env, nonGeneric)))
+          return t
+        } else if (head == '.') {
+          const [target, id] = tail
+          const t = analyse(target, env, nonGeneric)
+          return tenv[t.name](...t.tvars)[id] || fail(`Not found '${id}' in '${t.name}'`)
+        } else if (head == '-' && tail.length === 1) {
+          return tint
+        } else if (tail.length) {
+          const argv = tail.map(t => analyse(t, env, nonGeneric))
+          const rt = (env.__cache[JSON.stringify(tail)] ||= tvar()) // fix tvar
+          const ft = analyse(head, env, nonGeneric)
+          unify(tlambda(...argv, rt), ft)
+          return rt
+        } else {
+          return analyse(head, env, nonGeneric)
+        }
+      } else {
+        const v = node.toString()
+        return v.match(/^[0-9]/) ? tint :
+          v.match(/^["`]/) ? tstring :
+          env[v] ? fresh(env[v], nonGeneric) :
+          fail(`Not found '${v}' in env`, ...Object.keys(env).sort())
+      }
+    }
+
+    let tvarSequence = 0
+    const newt = o => (o.name||='', o.types||=[], o.tvars||=[], o)
+    const tvar = () => (name => newt({name, var: true}))((++tvarSequence).toString())
+    const tlambda = (...types) => types.length === 1 ? types[0] : newt({types})
+    const ttype = (name, ...tvars) => newt({name, tvars})
+    const tint = ttype('int')
+    const tstring = ttype('string')
+    const tbool = ttype('bool')
+    const tarray = t => ttype('array', t)
+    const tenv = {
+      string: () => ({
+        size: tint,
+        at: tlambda(tint, tstring),
+        contains: tlambda(tstring, tbool),
+        sub: tlambda(tstring, tstring, tstring),
+        rsub: tlambda(tstring, tstring, tstring),
+        split: tlambda(tstring, tarray(tstring)),
+        rsplit: tlambda(tstring, tarray(tstring)),
+      }),
+      array: t => ({
+        size: tint,
+        at: tlambda(tint, t),
+        contains: tlambda(t, tbool),
+        map: (u => tlambda(tlambda(t, u), tarray(u)))(tvar()),
+        keep: tlambda(tlambda(t, tbool), tarray(t)),
+        append: tlambda(t, tarray(t)),
+      }),
+    }
+    const v1 = tvar()
+    const v2 = tvar()
+    const topEnv = {
+      __cache: {},
+      '=': tlambda(v1, v1),
+      'if': tlambda(tbool, v1, v1, v1),
+      fail: tlambda(v1, v2),
+      catch: tlambda(v1, tlambda(v2, v1), v1)
+    }
+    const define = (s, t) => s.split(' ').map(x => topEnv[x] = t)
+    define('true false', tbool)
+    define('+ - * / % += -= *= /= %=', tlambda(tint, tint, tint))
+    define('< <= > >=', tlambda(tint, tint, tbool))
+    define('== !=', tlambda(v1, v1, tbool))
+    define('&& ||', tlambda(tbool, tbool, tbool))
+    for (const node of nodes.filter(node => node[0] == 'struct')) {
+      const name = node[1].toString()
+      const fields = node[node.length - 1].slice(1)
+      const d = {}
+      fields.map(([id, tname]) => d[id] = ttype(tname.toString()))
+      tenv[name] = () => d
+      topEnv[name] = tlambda(...fields.map(([id, tname]) => ttype(tname.toString())), ttype(name))
+    }
+    for (const node of nodes.filter(node => node[0] == 'adt')) {
+      const name = node[1].toString()
+      const fields = node[node.length - 1].slice(1)
+      const d = {}
+      fields.map(([id, tname]) => d[id] = tlambda(ttype(tname.toString()), ttype(name)))
+      tenv[name] = () => d
+      topEnv[name] = ttype(name)
+    }
+    return nodes.filter(node => node[0] == 'def').map(node => analyse(node, topEnv, []))
+  }
+  const nodes = parse()
+  infer(nodes)
+  const js = nodes.map(gen).join('\n')
+  return {js, nodes}
 }
 
 const test = () => {
@@ -88,10 +251,10 @@ const test = () => {
   }
   const exp = (expected, exp, ...defs) => {
     const src = defs.concat([`def main: ${exp}`]).join('\n')
-    const js = compile_to_js(src)
+    const js = compile(src).js
     const actual = run(js)
     if (JSON.stringify(expected) === JSON.stringify(actual)) {
-      process.stdout.write('.')
+      write('.')
     } else {
       puts('src:', src)
       puts('js:', js)
@@ -110,11 +273,11 @@ const test = () => {
   exp('\n', '`\\n`')
   exp([1, 2], '[1 2]')
   exp(1, '(n => n) 1')
-  exp(3, '(a,b => a + b) 1 2')
 
-  // method chain
+  // methods
+  exp('i', '"hi".at(1).at(0)')
   exp([2, 3], '[1 2].map(n => n + 1)')
-  exp([2, 3], '[1 2 3].map(n => n + 1).filter(n => n <= 3)')
+  exp([2, 3], '[1 2 3].map(n => n + 1).keep(n => n <= 3)')
 
   // int
   exp(-1, '(-1)')
@@ -132,13 +295,13 @@ const test = () => {
 
   // string with regular expression
   exp('h_o', '"hello".rsub `[el]+` "_"')
-  //exp(['1', '+', '2'], '"1 + 2".rsplit `([0-9\+])`')
+  //exp(['1', '+', '2'], '"1 + 2".rsplit(`([0-9\+])`).keep(s => s != "")') // fix me
 
   // array
   exp(2, '[1 2].size')
   exp([1, 2], '[1].append 2')
   exp([2, 3], '[1 2].map n => n + 1')
-  exp([1, 3], '[1 2 3].filter n => (n % 2) == 1')
+  exp([1, 3], '[1 2 3].keep n => (n % 2) == 1')
   exp(true, '[1 2].contains 1')
   exp(false, '[1 2].contains 3')
 
@@ -161,20 +324,22 @@ const test = () => {
   exp({x:1, y:2}, 'vector2 1 2', 'struct vector2:\n  x int\n  y int')
   exp(2, '(vector2 1 2).y', 'struct vector2:\n  x int\n  y int')
 
+  // TODO: enable after type inference implementation
   // algebraic data type
   exp({__tag: 'a', __value: 1}, 'ab.a 1', 'adt ab:\n  a int\n  b string')
-  exp(1, 'match (ab.a 1):\n  a v: v\n  b s: s.size', 'adt ab:\n  a int\n  b string')
-  exp(2, 'match (ab.b "hi"):\n  a v: v\n  b s: s.size', 'adt ab:\n  a int\n  b string')
+  //exp(1, 'match (ab.a 1):\n  a v: v\n  b s: s.size', 'adt ab:\n  a int\n  b string') // fix me
+  //exp(2, 'match (ab.b "hi"):\n  a v: v\n  b s: s.size', 'adt ab:\n  a int\n  b string') // fix me
 
-  // exp
+  // formula
   exp(3, '1 + 2')
   exp(7, '1 + 2 * 3')
   exp(5, '1 * 2 + 3')
   exp(true, '([1 2].size == 1 + 1) && [3 4].size == 2')
-  exp(1, '\n  var n 0\n  n = 1\n  n')
-  exp(true, '(s 1) == (s 1)', 'struct s: value int')
+  exp(true, '(s 1) == (s 1)', 'struct s:\n  value int')
+  exp(3, '(a,b => a + b) 1 2')
 
   // variable
+  exp(1, '\n  var n 0\n  n = 1\n  n')
   exp(3, '\n  var a 1\n  a += 2\n  a')
   exp(3, '\n  var a 1\n  def inc: a += 1\n  inc()\n  inc()\n  a')
 
@@ -195,6 +360,79 @@ const test = () => {
   // do block
   exp(1, 'do 1')
   exp(5, 'do 1 (2 + 3)')
+
+  // type inference
+  const type = (expected, exp, ...defs) => {
+    const src = defs.concat([`def main: ${exp}`]).join('\n')
+    const nodes = compile(src).nodes
+    const actual = showType(nodes[nodes.length - 1].type)
+    if (JSON.stringify(expected) === JSON.stringify(actual)) {
+      write('.')
+    } else {
+      puts('src:', src)
+      puts('expected:', expected)
+      puts('actual:', actual)
+      process.exit(1)
+    }
+  }
+  // primitives
+  type('int', '1')
+  type('string', '"hi"')
+  type('array[1]', '[]')
+  type('array[int]', '[1]')
+  type('(1 1)', 'n => n')
+  type('int', '(n => n) 1')
+  type('int', '(a,b => a) 1 "a"')
+  type('string', '(a,b => b) 1 "a"')
+  type('int', '(a,b => a + b) 1 2')
+
+  // methods
+  type('array[int]', '[1].map(n => n + 1)')
+  type('array[string]', '[1].map(n => "a")')
+  type('array[int]', '[1].map(n => n + 1).keep(n => n <= 3)')
+
+  // formula
+  type('int', '1 + 1')
+  type('int', '1 * 1')
+
+  // exp
+  type('int', '1 + 1')
+  type('bool', '1 > 1')
+
+  // branch
+  type('int', 'if true 1 2')
+  type('bool', 'if true true true')
+
+  // value
+  type('int', '(def value 1)')
+
+  // simple function
+  type('(int int)', '(def inc a (+ a 1))')
+  type('(int int int)', '(def add a b (+ a b))')
+
+  // generics
+  type('(1 1)', 'def f a: a')
+  type('(1 2 1)', 'def f a b: a')
+  type('(1 2 2)', 'def f a b: b')
+  type('int', 'f 1', 'def f a: a')
+  type('int', 'if (f true) (f 1) (f 2)', 'def f a: a')
+
+  // combinations
+  type('int', '(f 1) + (g 1)', 'def f x: x + 1', 'def g x: x + 2')
+  type('((1 2) (2 3) 1 3)', 'def _ f g x: g (f x)')
+  type('((1 2 3) (1 2) 1 3)', 'def _ x y z: x z (y z)')
+  type('(1 (1 bool) (1 1))', 'def _ b x: if (x b) x (x => b)')
+  type('(bool bool)', 'def _ x: if true x (if x true false)')
+  type('(bool bool bool)', 'def _ x y: if x x y')
+  type('(1 1)', 'def _ n : (x => x (y => y)) (f => f n)')
+  type('((1 2) 1 2)', 'def _ x y: x y')
+  type('((1 2) ((1 2) 1) 2)', 'def _ x y: x (y x)')
+  type('(1 ((1 2 3) 4 2) (1 2 3) 4 3)', 'def _ h t f x: f h (t f x)')
+  type('((1 1 2) ((1 1 2) 1) 2)', 'def _ x y: x (y x) (y x)')
+  type('(((1 1) 2) 2)', 'def _ y: id (y id)', 'def id x: x')
+  type('int', 'def f: if (id true) (id 1) (id 2)', 'def id x: x')
+  type('int', 'def g: (f true) + (f 4)', 'def f x: 3')
+  type('(bool (1 1))', 'h', 'def f x: x', 'def g y: y', 'def h b: if b (f g) (g f)')
 
   puts('ok')
 }
@@ -228,9 +466,9 @@ The commands are:
 	build       compile packages and dependencies
 	version     print Moa version\`)
 }`
-  let js = prefix + compile_to_js(moa) + suffix
+  let js = prefix + compile(moa).js + suffix
   fs.writeFileSync(__dirname + '/../bin/moa', js)
 }
 
 test()
-bootstrap()
+//bootstrap()
