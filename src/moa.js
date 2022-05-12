f207c57f3a5ec070e9e7163636d74e05505523fe
hiroshi <hiroshi@wovn.io>
2022-05-12 15:13:24 +0900

    update

--- src/moa.js
+++ src/moa.js
@@ -7,7 +7,8 @@ const put = (...a) => process.stdout.write(strs(a))
 const puts = (...a) => console.log(strs(a))
 const trace = (...a) => puts('TRACE', ...a)
 const dump = o => console.dir(o, {depth: null})
-const reserves = 'use fn struct enum let var if else elif for continue break return p pp'.split(' ')
+const reserves = 'use fn struct enum let var if else elif for while continue break return p pp'.split(' ')
+const fail = m => { throw Error(m) }
 
 Object.defineProperty(String.prototype, '_size', { get() { return this.length } });
 String.prototype._at = function (n) { return n >= this.length || n < -this.length ? fail('Out of index') : n >= 0 ? this[n] : this[this.length + n] }
@@ -22,7 +23,6 @@ Array.prototype._append = function (a) { return this.concat(a) }
 Array.prototype._contains = function (s) { return this.includes(s) }
 Array.prototype._keep = function (f) { return this.filter(f) }
 Object.defineProperty(Boolean.prototype, '_flip', { get() { return this == false } })
-
 
 const isOp2 = s => s && s.match && s.match(/^[+\-*%/=><|&.]+$/)
 const compile = source => {
@@ -82,7 +82,8 @@ const parse = tokens => {
     if (t.match(/^[0-9](\.[0-9]+)?$/) || t.match(/^[A-Za-z0-9_]+$/) || t.startsWith('"')) {
       return t
     } else if (t.match(/^[A-Za-z0-9_]+\($/)) {
-      return [t.slice(0, -1),  ...many(exp, {stop: u => u === ')'})]
+      const args =  many(exp, {stop: u => u === ')'})
+      return args.length ? [t.slice(0, -1),  ...args] : t + ')'
     } else if ('}]);'.includes(t.toString())) {
       return t
     } else if (t === '(') {
@@ -100,6 +101,11 @@ const parse = tokens => {
 
 const generate = nodes => {
   const gen = o => Array.isArray(o) ?(o.length === 1 ? gen(o[0]) : apply(o)) : o
+  const isCond = args => ['if', 'unless'].includes(args[args.length - 2])
+  const cond = (head, args) => args.length === 0 ? head :
+    args[0] === 'if' ? `if (${gen(args[1])}) ${head}` :
+    args[0] === 'unless' ? `if (!${gen(args[1])}) ${head}` :
+    fail(`Unknown condition ${args}`)
   const apply = ([head, ...args]) => {
     if (isOp2(head)) {
       if (head === '.') {
@@ -126,10 +132,34 @@ const generate = nodes => {
       return `else if (${gen(args[0])}) ${gen(args[1])} ${args.length >= 3 ? gen(args.slice(2)) : ''}`
     } else if (head === 'else') {
       return `else ${gen(args)}`
-    } else if (head === 'p') {
-      return `__p(${args.map(gen)})`
-    } else if (head === 'pp') {
-      return `__pp(${args.map(gen)})`
+    } else if (head === 'for') {
+      if (args.length == 3) {
+        return `for (let ${args[0]} of ${gen(args[1])}) { ${gen(args[2])} }`
+      } else {
+        throw Error(`Unknown for syntax ${args}`)
+      }
+    } else if (head === 'while') {
+      return `while (${gen(args[0])}) { ${gen(args[1])} }`
+    } else if (head === 'continue' || head === 'break') {
+      return cond(head, args)
+    } else if (head === 'return') {
+      if (args.length === 0) {
+        return 'return'
+      } else if (args.length === 1) {
+        return `return ${gen(args[0])}`
+      } else if (args.length === 2) {
+        return cond('return', args)
+      } else if (args.length === 3) {
+        return cond(`return ${args[0]}`, args.slice(1))
+      } else {
+        throw Error(`Unknown return syntax ${args}`)
+      }
+    } else if (head === 'p' || head === 'pp') {
+      if (isCond(args)) {
+        return cond(`__${head}(${args.slice(0, -2).map(gen)})`, args.slice(-2)) + '; ""'
+      } else {
+        return `__${head}(${args.map(gen)}); ""`
+      }
     } else {
       return `${head}(${args.map(gen)})`
     }
@@ -142,8 +172,8 @@ const test = () => {
     let actual
     try {
       const __stdout = []
-      const __p = (...a) => {__stdout.push(a.map(str).join(' ')); return ''}
-      const __pp = (...a) => {__stdout.push(a.map(x => JSON.stringify(x, null, 2)).join(' ')); return ''}
+      const __p = (...a) => __stdout.push(a.map(str).join(' '))
+      const __pp = (...a) => __stdout.push(a.map(x => JSON.stringify(x, null, 2)).join(' '))
       actual = eval(js)
       if (__stdout.length) {
         actual += __stdout.join('\n')
@@ -164,6 +194,7 @@ const test = () => {
       process.exit(1)
     }
   }
+
   // define:
   // | "let" id exp
   exp(1, 'let a 1; a')
@@ -171,6 +202,7 @@ const test = () => {
   exp(3, 'var a 1; a += 2; a')
   // | "fn" id+ exp
   exp(3, 'fn add a b a + b; add(1 2)')
+  exp(1, 'fn f 1; f()')
   // | "struct" id+ "{" (define lf)* } "}"
   exp(11, 'struct item { name string; price int }; item("jar" 11).price')
 
@@ -186,14 +218,29 @@ const test = () => {
   exp(2, 'var a; if false { a = 1 } elif true { a = 2 } else { a = 3 }; a')
   exp(3, 'var a; if false { a = 1 } elif false { a = 2 } else { a = 3 }; a')
   // | "for" id exp exp
+  exp(3, 'var a 0; for i [1 2] { a += i }; a')
   // | "while" exp exp
+  exp(3, 'var a 0; while a < 3 { a += 1 }; a')
   // | "continue" cond?
+  exp(0, 'var a 0; for i [1 2 3] { continue; a += i }; a')
+  exp(4, 'var a 0; for i [1 2 3] { continue if i == 2; a += i }; a')
+  exp(4, 'var a 2; for i [1 2 3] { continue unless i == 2; a += i }; a')
   // | "break" cond?
+  exp(0, 'var a 0; for i [1 2 3] { break; a += i }; a')
+  exp(1, 'var a 0; for i [1 2 3] { break if i == 2; a += i }; a')
+  exp(0, 'var a 0; for i [1 2 3] { break unless i == 2; a += i }; a')
   // | "return" exp? cond?
+  exp(1, 'fn f { return 1; return 2 }; f()')
+  exp(2, 'fn f { return 1 if false; return 2 }; f()')
+  exp(1, 'fn f { return 1 unless false; return 2 }; f()')
   // | "p" exp+ cond?                 # print one line while developing
   exp('1 [2,3]\n4', 'p 1 [2 3]; p 4')
+  exp('1', 'p 1 if true')
+  exp('', 'p 1 unless true')
   // | "pp" exp+ cond?                # print multiple lines while developing
   exp('1 [\n  2,\n  3\n]\n4', 'pp 1 [2 3]; pp 4')
+  exp('1', 'pp 1 if true')
+  exp('', 'pp 1 unless true')
   // exp: node (op2 exp)*
   exp(3, '1 + 2')
   exp(7, '1 + 2 * 3')
