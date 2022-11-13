301f799e0c20b1b96d180390542a9fbb36dc9962
hiroshi <hiroshi@wovn.io>
2022-11-13 17:02:10 +0900

    Support variable arguments for list

--- mini/infer.js
+++ mini/infer.js
@@ -9,6 +9,7 @@ const put = (...a) => { process.stdout.write(a.map(str).join(' ')); return a[0] 
 const puts = (...a) => { console.log(a.map(str).join(' ')); return a[0] }
 const infer = root => {
   let unique = 1
+  const repeat = t => (t.repeatable = true, t)
   const tvar = () => (tid => ({tid, instance: null, toString: () => tid.toString()}))(unique++)
   const tclass = name => ({name, instance: null, toString: () => name})
   const type = (name, ...generics) => ({name, generics, toString: () => `${name}${generics.length ? `(${generics.map((g, i) => g.instance ? g.instance.toString() : i).join(' ')})` : ''}`})
@@ -23,7 +24,7 @@ const infer = root => {
     '+': () => (t => [t, t, t])(type('num')),
     'int': () => [type('num'), tint],
     'real': () => [type('num'), treal],
-    'list': () => (t => [t, type('list', t)])(tvar()),
+    'list': () => (t => [t, type('list', t)])(repeat(tvar())),
     '__empty': () => type('list', tvar()),
   }
   const inferTop = (node, env) => {
@@ -31,23 +32,19 @@ const infer = root => {
     const unify = (l, r, f) => {
       l = prune(l)
       r = prune(r)
-      if (Array.isArray(l)) {
-        unify(l[0], r)
-        return l.length === 2 ? l[1] : l.slice(1)
-      }
       const narrow = (ts, target) => ts.find(t => t.toString() === target.toString()) || fail(`No compatible '${ts}' '${target}'`)
-      const assign = (base, target) => {
-        return base.instance = target
-      }
-      return l.tid && r.tid ? assign(r, l) :
-        l.tid ? assign(l, r) :
-        r.tid ? assign(r, l) :
+      return Array.isArray(l) && l[0].repeatable && unify(l[0], r, () => false) ? (prune(l[0]), l) :
+        Array.isArray(l) ? (unify(l[0], r), l.length === 2 ? l[1] : l.slice(1)) :
+        l.tid && r.tid ? r.instane = l :
+        l.tid ? l.instance = r :
+        r.tid ? r.instance = l :
         l.toString() === r.toString() ? l :
         l.name in tclasses ? l.instance = narrow(tclasses[l.name], r) :
         r.name in tclasses ? r.instance = narrow(tclasses[r.name], l) :
         f ? f() : fail(`Unmatch ${l} and ${r}`)
     }
-    const apply = ([head, ...argv]) => argv.reduce((ret, x) => unify(ret, inf(x)), inf(head))
+    const apply = ([head, ...argv]) => derepeat(argv.reduce((ret, x) => unify(ret, inf(x)), inf(head)))
+    const derepeat = a => Array.isArray(a) && a[0].repeatable ? derepeat(a.slice(1)) : a
     const value = v => v.match(/^[0-9]+$/) ? tclass('num') :
       v.match(/^[0-9]+\.[0-9]+$/) ? treal :
       v in env ? env[v]() :
@@ -71,6 +68,9 @@ if (require.main === module) {
   test('list(0)', '[]')
   test('list(num)', '[1]')
   test('list(real)', '[1.0]')
+  test('list(num)', '[1 2]')
+  test('list(real)', '[1 2.0]')
+  test('list(real)', '[1 2.0 3]')
 
   // type cast
   test('int', 'int(1)')
