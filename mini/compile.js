e344230b139931fe827ee8e8016bfb7157dfb852
hiroshi <hiroshi@wovn.io>
2022-11-27 17:32:49 +0900

    support user defined algebraic data type

--- mini/compile.js
+++ mini/compile.js
@@ -19,21 +19,34 @@ const compile = root => {
     '__call,dict': '({})',
   }
   const struct = (name, fields) => `const ${name} = (${fields.map(f => f[0])}) => ({${fields.map(f => f[0])}})`
+  const adt = (name, fields) => fields.map(f => Array.isArray(f) ? adtValue(f[0], f[1]) : adtTag(f)).join('\n') + `\nconst ${name} = ({${fields.map(f => f[0])}})`
+  const adtTag = name => `const ${name} = ({__tag: '${name}'})`
+  const adtValue = (name, v) => `const ${name} = __val => ({__tag: '${name}', __val})`
   const flat = a => Array.isArray(a) && a.length == 1 && a[0][0] == '__do' ? a[0].slice(1).map(flat) : a
+  const switch_ = (t, cs) => `(__target => ${cs.map(c => Array.isArray(c) ? caseValue(c) : caseTag(c)).join(' : ')} : (() => {throw new Error("switch unmatch")})() )(${js(t)})`
+  const caseValue = c => `__target.__tag === '${c[2][0]}' ? (${c[2][1]} => ${js(c[3])})(__target.__val)`
+  const caseTag = c => `__target.__tag === '${c[2]}' ? ${js(c[3])}`
   const value = x => x in map ? map[x] :
     x.startsWith('r"') ? `(new RegExp(${x.slice(1)}))` :
     x.toString()
   const apply = ([h,...t]) =>
     h == 'list' ? `[${t.map(js).join(',')}]` :
+    h == 'string' ? `(${js(t)}).toString()` :
     h == 'set' ? `[${t.map(js).join(',')}]` :
     h == 'tuple' ? `[${t.map(js).join(',')}]` :
     h == 'dict' ? '({' + [...Array(t.length / 2).keys()].map(i => `[${t[i*2]}]: ${compile(t[i*2+1])}`).join(', ') + '})' :
     h == '.' && t[1].match(/^[0-9]+$/) ? `${compile(t[0])}[${t[1]}]` : // tuple(...).1
-    h == '__do' ? t.map(js).join('\n') :
+    h == '__do' ? t.map(js).join(';\n') :
     h == ':' && t[0] == 'struct' ? struct(t[1], flat(t.slice(2))) :
+    h == ':' && t[0] == 'adt' ? adt(t[1], flat(t.slice(2))) :
+    h == ':' && t[0] == 'switch' ? switch_(js(t[1]), flat(t.slice(2))) :
     h == '.' ? compile(t[0]) + '.' + t[1] :
     `${h}(${t.map(js).join(',')})`
-  const js = x => Array.isArray(x) ? (x in map ? map[x] : apply(x)) : value(x)
+  //const js = x => Array.isArray(x) ? (x in map ? map[x] : apply(x)) : value(x)
+  const js = x => !Array.isArray(x) ? value(x) : 
+    x.length === 1 ? js(x[0]) :
+    x in map ? map[x] :
+    apply(x)
   return js(root)
 }
 
@@ -50,6 +63,7 @@ if (require.main === module) {
   test(false, 'false')
   test(1, '1')
   test(1.2, '1.2')
+  test('1', 'string(1)')
   test('hi', '"hi"')
   test('hi', '`hi`')
   test(/hi/, 'r"hi"')
@@ -70,6 +84,12 @@ if (require.main === module) {
   // user defined type
   test('moa', 'struct item:\n  name string\nitem("moa").name')
   test(1, 'struct item:\n  name string\n  price int\nitem("moa" 1).price')
+
+  // user defined algebraic data type
+  test(1, 'adt ab:\n  a\n  b\nswitch a:\n  case a: 1\n  case b: 2')
+  test('hi', 'adt ab:\n  a string\n  b int\nswitch a "hi":\n  case a s: s\n  case b n: string(n)')
+  test('hi', 'adt ab:\n  a string\n  b int\nswitch a "hi":\n  case a s: s\n  case b n: string(n)')
+  test('1', 'adt ab:\n  a string\n  b int\nswitch b 1:\n  case a s: s\n  case b n: string(n)')
 
   puts('ok')
 }
