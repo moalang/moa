03d62c135a689386bc1c192f5dfd00f4437212e5
hiroshi <hiroshi@websoft.jp>
2024-01-24 00:00:28 +0900

    Enable syntax sugar for prioritized binary operators

--- src/moa.js
+++ src/moa.js
@@ -1,28 +1,37 @@
 'use strict'
+const util = require('util')
 const print = (...a) => (console.log(...a), a[0])
-const log = (...a) => (console.error(...a), a[0])
+const log = (...a) => (console.error(...a.map(o => util.inspect(o, false, null, true))), a[0])
 
 const execute = (source, embedded) => {
   // parser
   let offset = 0
-  const simplify = o => Array.isArray(o) ?  o.map(simplify) : o.code
-  const show = o => log(JSON.stringify(simplify(o), null, 2))
   const tokens = source.split(/([()]|\s+)/).flatMap(code => {
     offset += code.length
     return code.trim() ? [{code, offset}] : []
   })
   let pos = 0
   // TODO: syntax desugar
-  // - [ ] a + b     -> (+ a b)
-  // - [ ] a + b * c -> (+ a (* b c))
-  // - [ ] a b       -> (a b)
+  // - [x] a + b     -> (+ a b)
+  // - [x] a + b * c -> (+ a (* b c))
+  // - [x] a b       -> (a b)
   // - [ ] a b\nc d  -> (a b) (c d)
   // - [ ] a b; c d  -> (a b) (c d)
+  //const list = a => (t => t.code === ')' ? a : list(a.concat([t])))(unit())
+  //const unit = () => (t => t.code === '(' ? list([]) : t)(tokens[pos++])
+  const op2s = '|| && == != < <= > >= + - * / % ^ **'.split(' ') // low...high, other operators are lowest
+  const priority = t => op2s.findIndex(op => op === t.code)
+  const isOp2 = t => t && /^[+\-*\/%<>!=~.]/.test(t.code)
+  const reorder = (a, b) => a.length <= 2 ? b.concat(a) :
+    !isOp2(a[1]) ? reorder(a.slice(1), b.concat(a.slice(0, 1))) :
+    Array.isArray(a[0]) && isOp2(a[0][0]) && priority(a[1]) > priority(a[0][0]) ?
+    reorder([[a[0][0], a[0][1], [a[1], a[0][2], a[2]]]].concat(a.slice(3)), b) :
+    reorder([[a[1], a[0], a[2]]].concat(a.slice(3)), b)
   const list = a => (t => t.code === ')' ? a : list(a.concat([t])))(unit())
-  const unit = () => (t => t.code === '(' ? list([]) : t)(tokens[pos++])
-  const top = []
+  const unit = () => (t => t.code === '(' ? reorder(list([]), []) : t)(tokens[pos++])
+  const nodes = []
   while (pos < tokens.length) {
-    top.push(unit())
+    nodes.push(unit())
   }
 
   // interpriter
@@ -55,12 +64,13 @@ const execute = (source, embedded) => {
     ['%', (l, r) => l % r],
     ['||', (l, r) => l || r],
     ['&&', (l, r) => l && r],
+    ['**', (l, r) => l ** r],
   ].map(([op, opf]) => {
     embedded[op] = (env, [head, ...a]) => a.reduce((acc, x) => opf(acc, run(env, x)) , run(env, head)),
     embedded[op + '='] = (env, [l, r]) => env[l.code] = opf(run(env, l), run(env, r))
   })
   try {
-    return top.map(node => run(embedded, node)).at(-1)
+    return nodes.map(node => run(embedded, node)).at(-1)
   } catch (e) {
     log(e.stack)
     process.exit(1)
