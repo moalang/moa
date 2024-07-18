1bf49706e1743d6624f8efea9d9f69df2ba227fc
hiroshi <hiroshi@wovn.io>
2024-07-18 20:02:39 +0900

    refactoring

--- src/runtime.js
+++ src/runtime.js
@@ -1,12 +1,12 @@
-const __show = o => typeof o === 'string' ? o :
-  o instanceof Array ? `(list ${o.map(__show).join(' ')})` :
-  o instanceof Map ? `(dict ${[...o].map(__show).join(' ')})` :
-  o instanceof Set ? `(dict ${[...o].map(__show).join(' ')})` :
+const __string = o => typeof o === 'string' ? o :
+  o instanceof Array ? `(list ${o.map(__string).join(' ')})` :
+  o instanceof Map ? `(dict ${[...o].map(__string).join(' ')})` :
+  o instanceof Set ? `(dict ${[...o].map(__string).join(' ')})` :
   o.toString()
-const __io_put = (...a) => process.stdout.write(a.map(__show).join(' '))
-const __io_puts = (...a) => process.stdout.write(a.map(__show).join(' ') + '\n')
-const __fail = (m, ...a) => { const e = new Error(m); e.detail = a; throw e }
-const assert = (a, b) => __show(a) === __show(b) ? process.stdout.write('.') : __fail(`\`${__show(a)}\` is not \`${__show(b)}\``, a, b)
+const __throw = (m, d) => { const e = new Error(m); e.detail = d; throw e }
+const __dict_set = (m, k, v) => (m.set(k, v), v)
+const assert = (a, b) => __string(a) === __string(b) ? process.stdout.write('.') : __throw(`\`${__string(a)}\` is not \`${__string(b)}\``, a, b)
+const tuple = (...a) => a
 const list = (...a) => a
 const dict = (...a) => new Map([...Array(a.length/2)].map((_,i) => [a[i*2], a[i*2+1]]))
-const __dict_set = (m, k, v) => (m.set(k, v), v)
+const log = (...a) => (console.log(...a.map(__string)), a.at(-1))
