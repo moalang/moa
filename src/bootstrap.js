096ac87d9d209a2b3dbdb131636a5f9c529648ea
hiroshi <hiroshi@wovn.io>
2024-09-27 15:19:51 +0900

    Fix test in moa

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,14 +1,10 @@
 'use strict'
 
-const STMT = '__statement'
-const STMT_TOKEN = {text: STMT, pos: 0, line: 0}
-const BLOCKS = new Set('def test'.split(' '))
-
 function run(source, env) {
   return evaluate(parse(tokenize(source)), env || {})
 }
 
-function evaluate(node, env) {
+function evaluate(root, env) {
   function rec(node) {
     return evaluate(node, env)
   }
@@ -17,10 +13,6 @@ function evaluate(node, env) {
     return evaluate(node, {...env, ...Object.fromEntries(params)})
   }
   function eq(a, b) {
-    const ta = typeof a
-    const tb = typeof b
-    const aa = Array.isArray(a)
-    const ab = Array.isArray(b)
     return typeof a === typeof b && Array.isArray(a) && Array.isArray(b) &&
       Array.isArray(a) ? a.length === b.length && a.every((x, i) => eq(x, b[i])) :
       typeof a === 'object' ? eq(Object.keys(a).sort(), Object.keys(b).sort()) && Object.keys(a).every(k => eq(a[k], b[k])) :
@@ -46,20 +38,26 @@ function evaluate(node, env) {
     string: s => ({
       size: s.length,
       has(ss) { return s.includes(ss) },
+      int: parseInt(s),
+      float: parseFloat(s),
     })
   }
   function prop(o, name) {
     const table = props[typeof o](o)
     return name.text in table ? table[name.text] : fail(`no field '${name.text}' of '${typeof o}'`, name)
   }
+  function line(caller) {
+    return Array.isArray(caller) ? line(caller[0]) : caller.line
+  }
   const tester = {
-    eq(a, b) {
-      return eq(a, b) || fail(`'${a}' != '${b}'`)
+    eq(a, b, caller) {
+      return eq(a, b) || fail(`${line(caller)}: '${a}' != '${b}'`)
     }
   }
-  if (Array.isArray(node)) {
-    const head = node[0]
-    const tail = node.slice(1)
+  if (Array.isArray(root)) {
+    const head = root[0]
+    const tail = root.slice(1)
+    if (head.text==="test") log(root)
     switch (head.text) {
       case '!': return !rec(tail[0])
       case '.': return prop(rec(tail[0]), tail[1])
@@ -76,52 +74,84 @@ function evaluate(node, env) {
       case '*': return rec(tail[0]) * rec(tail[1])
       case '/': return rec(tail[0]) / rec(tail[1])
       case '%': return rec(tail[0]) % rec(tail[1])
-      case STMT: return tail.map(rec).at(-1)
-      case 'def': env[tail[0].text] = (...args) => recWith(tail.at(-1), args, tail.slice(1, -1)); break
+      case '__stmt': return tail.map(rec).at(-1)
+      case 'def': return env[tail[0].text] = (...args) => recWith(tail.at(-1), args, tail.slice(1, -1))
       case 'test': return recWith(tail.at(-1), [tester], tail.slice(0, -1))
       default:
         const f = Array.isArray(head) ? rec(head) : head.text in env ? env[head.text] : fail(`no id '${head.text}'`, head)
-        return f(...tail.map(rec))
+        return f(...tail.map(rec), head)
     }
   } else {
-    const t = node.text
+    const t = root.text
     return /^[0-9]+\./.test(t) ? parseFloat(t) :
       /^[0-9]/.test(t) ? parseInt(t) :
       t[0] === '"' ? t.slice(1, -1) :
       t === 'true' ? true :
       t === 'false' ? false :
       t in env ? env[t] :
-      fail(`no id '${t}'`, node)
+      fail(`no id '${t}'`, root)
   }
 }
 
 function parse(tokens) {
   const len = tokens.length
   let index = 0
-  function consumeBottom() {
+  function bottom() {
     const node = consume()
     switch (node.text) {
       case '!':
-        return [node, consumeUnit()]
+        return [node, exp()]
       case '(':
-        const ret = consumeUnit()
+        const ret = exp()
         consume().text !== ')' && fail('( is not close', node)
         return ret
-      default:
-        if (BLOCKS.has(node.text)) {
-          const a = [node].concat(consumeLine(node.line, t => t.text === ':'))
-          if (tokens[index].line === node.line) {
-            return a.concat([consumeUnit()])
-          } else {
-            return a.concat([consumeBlock(tokens[index].indent)])
-          }
+      case 'def':
+      case 'test':
+        const a = [node].concat(line(node.line, t => t.text === ':'))
+        if (tokens[index].line === node.line) {
+          return a.concat([exp()])
         } else {
-          return node
+          return a.concat([block(tokens[index])])
         }
+      default:
+        return node
     }
   }
-  function consumeUnit() {
-    return consumeWith(consumeBottom())
+  function exp() {
+    return consumeWith(bottom())
+  }
+  function line(line, f) {
+    const a = []
+    while (index < len && tokens[index].line === line) {
+      const t = consume()
+      if (f(t)) {
+        break
+      } else {
+        a.push(t)
+      }
+    }
+    return a
+  }
+  function block(base) {
+    const line = base.line
+    const indent = base.indent
+    const a = [{text: '__stmt', line, indent}]
+    while (index < len && tokens[index].indent === indent) {
+      a.push(exp())
+    }
+    return a
+  }
+  function until(start, mark) {
+    const a = []
+    while (index < len) {
+      const token = exp()
+      if (token.text === mark) {
+        return a
+      } else {
+        a.push(token)
+      }
+    }
+    fail(`no close '${start.text}'`, start)
   }
   function consumeWith(node) {
     if (index < len) {
@@ -140,7 +170,7 @@ function parse(tokens) {
         case '/':
         case '%':
           const op2 = consume()
-          return [op2, node, consumeUnit()]
+          return [op2, node, exp()]
         case '.':
           const dot = consume()
           const ref = [dot, node, consume()]
@@ -159,45 +189,14 @@ function parse(tokens) {
   function consume() {
     return tokens[index++] || fail('EOT', tokens.at(-1))
   }
-  function consumeLine(line, f) {
-    const a = []
-    while (index < len && tokens[index].line === line) {
-      const t = consume()
-      if (f(t)) {
-        break
-      } else {
-        a.push(t)
-      }
-    }
-    return a
-  }
-  function consumeBlock(indent) {
-    const a = []
-    while (index < len && tokens[index].indent === indent) {
-      a.push(unit())
-    }
-    return a
-  }
-  function until(start, mark) {
-    const a = []
-    while (index < len) {
-      const token = consume()
-      if (token.text === mark) {
-        return a
-      } else {
-        a.push(token)
-      }
-    }
-    fail(`no close '${start.text}'`, start)
-  }
   const nodes = []
   while (index < len) {
-    nodes.push(consumeUnit())
+    nodes.push(exp())
   }
   if (index < tokens.length) {
     fail('failed to parse', `${ index } < ${ tokens.length }`)
   }
-  return nodes.length === 1 ? nodes[0] : [STMT_TOKEN].concat(nodes)
+  return nodes.length === 1 ? nodes[0] : [{text: '__stmt', line: 1, indent: 0}].concat(nodes)
 }
 
 function tokenize(source) {
@@ -268,7 +267,7 @@ function fail(message, info) {
 }
 
 function log(...a) {
-  console.log(...a)
+  console.dir(a, {depth: null})
   return a[0]
 }
 
--- src/bootstrap.test.js
+++ src/bootstrap.test.js
@@ -17,6 +17,8 @@ tester((eq, only) => {
   // Method
   eq(1, 'a.b', {a: {b: 1}})
   eq(1, '"a".size')
+  eq(1, '"1".int')
+  eq(1.2, '"1.2".float')
 
   // Unry operator
   eq(false, '!true')
@@ -49,19 +51,46 @@ tester((eq, only) => {
   eq(1, 'f(1)', {f: n => n})
   eq(1, 'def f: 1\nf()')
   eq(1, 'def f n: n\nf(1)')
+  eq('a', 'def f n: n\nf("a")')
 
-  // test
+  // Test
   eq(true, 'test t: t.eq(1 1)')
+  eq(new Error(`1: '1' != '2'`), 'test t: t.eq(1 2)')
+  eq(new Error(`3: '1' != '2'`), 'test t:\n  t.eq(1 1)\n  t.eq(1 2)')
+
+  // Complex expression
+  eq(1, 'f(f(1))', {f: n => n})
+
+  // moa.moa
+  const fs = require('node:fs')
+  const moa = fs.readFileSync('moa.moa', 'utf-8')
+  eq(true, moa)
 })
 
 function tester(f) {
   const test = require('node:test')
   const assert = require('node:assert').strict
   const {run} = require('./bootstrap')
+  function summary(s) {
+    return s.includes('\n') ? s.split('\n')[0] + '...' : s
+  }
   function noop() {}
   function eq(expected, source, env) {
     const line = new Error().stack.split('\n')[2].split(':').at(-2)
-    test(`${line}: ${expected} == ${source}`, () => assert.equal(run(source, env), expected, source))
+    if (expected instanceof Error) {
+      test(`${line}: Error(${expected.message}) == ${summary(source)}`, () => {
+        try {
+          run(source, env)
+          assert.fail('no exception')
+        } catch (e) {
+          assert.equal(e.message, expected.message, source)
+        }
+      })
+    } else {
+      test(`${line}: ${expected} == ${summary(source)}`, () => {
+        assert.equal(run(source, env), expected, source)
+      })
+    }
   }
   if (/\n *only\(/.test(f.toString())) {
     f(noop, eq)
