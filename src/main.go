f1a0dfd002b8dc4a42288504fb8185b55b7c16fb
hiroshi <hiroshi@wovn.io>
2024-12-27 17:57:36 +0900

    wip

--- src/main.go
+++ src/main.go
@@ -7,6 +7,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"runtime"
+	"slices"
 	"strings"
 )
 
@@ -69,12 +70,10 @@ func runGoCommand(gocode string, args ...string) {
 }
 
 func compileToGoCode(args []string) string {
-	codes := readMoaCodes(args[1:])
-	ast := parseMoaCode(strings.Join(codes, "\n"))
-	return generateGoCode(ast, args[0] == "test")
+	return generateGoCode(parseMoaCodes(args[1:]), args[0] == "test")
 }
 
-func readMoaCodes(args []string) []string {
+func parseMoaCodes(args []string) []AST {
 	if len(args) == 0 {
 		args = append(args, ".")
 	}
@@ -95,15 +94,97 @@ func readMoaCodes(args []string) []string {
 			targetFiles = append(targetFiles, arg)
 		}
 	}
-	codes := []string{}
+	asts := []AST{}
 	for _, file := range targetFiles {
 		buf, err := os.ReadFile(file)
 		if err != nil {
 			panic(err)
 		}
-		codes = append(codes, string(buf))
+		asts = append(asts, parse(tokenize(string(buf), file))...)
 	}
-	return codes
+	return asts
+}
+
+func parse(tokens []Token) []AST {
+	return []AST{makeAST("def", makeAST("main"), makeAST("puts", makeAST("\"hello\"")))}
+}
+
+func tokenize(code string, file string) []Token {
+	tokens := []Token{}
+	i := 0
+	lineno := 1
+	br := 0
+	units := strings.Split("! ~ * / % + - | & ^ > < = ( ) [ ] { } .", " ")
+	op2s := strings.Split("?? << >> || && >= == != <= += -= *= /= %= |= &= ^=", " ")
+	push := func(code string) {
+		tokens = append(tokens, Token{
+			File:   file,
+			Code:   code,
+			Lineno: lineno,
+			Column: i - br + 1,
+		})
+	}
+	for i = 0; i < len(code); i++ {
+		if code[i] == '\n' {
+			lineno += 1
+			br = i
+		} else if code[i] == ' ' {
+			// ignore space
+		} else if slices.Contains(op2s, code[i:i+2]) {
+			push(code[i : i+2])
+			i++
+		} else if slices.Contains(units, code[i:i+1]) {
+			push(code[i : i+1])
+		} else if '0' <= code[i] && code[i] <= '9' {
+			left := i
+			for i < len(code) && (('0' <= code[i+1] && code[i+1] <= '9') || code[i+1] == '.') {
+				i++
+			}
+			push(code[left : i+1])
+		} else if ('A' <= code[i] && code[i] <= 'Z') || ('a' <= code[i] && code[i] <= 'z') || code[i] == '_' {
+			left := i
+			for i < len(code) && (('A' <= code[i+1] && code[i+1] <= 'Z') || ('a' <= code[i+1] && code[i+1] <= 'z') || code[i+1] == '_') {
+				i += 1
+			}
+			push(code[left : i+1])
+		} else if code[i] == '"' {
+			left := i
+			i++
+			for i < len(code) && code[i] != '"' && code[i-1] != '\\' {
+				i++
+			}
+			s := code[left : i+1]
+			s = strings.Replace(s, "\\n", "\n", -1)
+			s = strings.Replace(s, "\\t", "\t", -1)
+			push(s)
+		} else {
+			panic("Unknown character `" + code[i:i+1] + "`")
+		}
+	}
+
+	//fmt.Println(tokens)
+	return tokens
+}
+
+func makeAST(code string, args ...AST) AST {
+	return AST{Token: Token{Code: code}, Args: args, Type: Type{TypeName: "any"}}
+}
+
+func generateGoCode(asts []AST, isTest bool) string {
+	codes := []string{`package main
+import "fmt"
+func moa_puts(a ...any) {
+  fmt.Println(a...)
+}`}
+	if isTest {
+		codes = append(codes, "\nfunc main() { fmt.Println(\"...............................ok\") }")
+	} else {
+		codes = append(codes, "\nfunc main() { moa_main() }")
+		for _, ast := range asts {
+			codes = append(codes, ast.Gen())
+		}
+	}
+	return strings.Join(codes, "\n")
 }
 
 type Type struct {
@@ -111,29 +192,37 @@ type Type struct {
 	Instance *Type
 }
 
-type AST struct {
-	Code   string
-	Args   []AST
-	Type   Type
+type Token struct {
 	File   string
-	Line   int
+	Code   string
+	Lineno int
 	Column int
+}
+
+type AST struct {
+	Token Token
+	Args  []AST
+	Type  Type
 }
 
 func (a AST) String() string {
 	if len(a.Args) == 0 {
-		return a.Code
+		return a.Token.Code
 	} else {
-		return fmt.Sprintf("%s%v", a.Code, a.Args)
+		return fmt.Sprintf("%s%v", a.Token.Code, a.Args)
 	}
 }
 
+func (t Token) String() string {
+	return t.Code
+}
+
 func (a AST) Gen() string {
-	if a.Code == "def" {
+	if a.Token.Code == "def" {
 		name := a.Args[0]
 		args := []string{}
 		for _, arg := range a.Args[0].Args {
-			args = append(args, arg.Code+" "+arg.GoType())
+			args = append(args, arg.Token.Code+" "+arg.GoType())
 		}
 		body := []string{}
 		for _, arg := range a.Args[1:] {
@@ -141,9 +230,9 @@ func (a AST) Gen() string {
 		}
 		return fmt.Sprintf(`func moa_%s(%s) { %s }`, name, strings.Join(args, ", "), strings.Join(body, "\n"))
 	} else if len(a.Args) == 0 {
-		return a.Code
+		return a.Token.Code
 	} else {
-		s := "moa_" + a.Code + "("
+		s := "moa_" + a.Token.Code + "("
 		for i, arg := range a.Args {
 			if i > 0 {
 				s += ","
@@ -160,25 +249,4 @@ func (a AST) GoType() string {
 		t = t.Instance
 	}
 	return t.TypeName
-}
-
-func parseMoaCode(code string) AST {
-	return makeAST("def", makeAST("main"), makeAST("puts", makeAST("\"hello\"")))
-}
-
-func makeAST(code string, args ...AST) AST {
-	return AST{Code: code, Args: args, Type: Type{TypeName: "any"}}
-}
-
-func generateGoCode(ast AST, isTest bool) string {
-	prefix := `package main
-import "fmt"
-func moa_puts(a ...any) {
-  fmt.Println(a...)
-}`
-	if isTest {
-		return prefix + "\nfunc main() { fmt.Println(\"...............................ok\") }"
-	} else {
-		return prefix + "\nfunc main() { moa_main() }\n" + ast.Gen()
-	}
 }
