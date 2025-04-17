45533a81fb5077c1ee5eea92b72f0e7030219836
hiroshi <hiroshi@websoft.jp>
2025-04-18 00:09:37 +0900

    wip: enable compiler

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -14,11 +14,23 @@ const showToken = t => t.code + "\t" + [t.op2 && "op2", t.op1 && "op1", t.call &
 
 const runtime = (() => {
   "use strict"
-  Object.defineProperty(String.prototype, "present", { get() { return this.length > 0 } })
+  Object.defineProperty(Array.prototype, "size", { get() { return this.length } })
+  Object.defineProperty(Array.prototype, "present", { get() { return this.length > 0 } })
   Object.defineProperty(String.prototype, "size", { get() { return this.length } })
+  Object.defineProperty(String.prototype, "present", { get() { return this.length > 0 } })
   Object.defineProperty(String.prototype, "has", { get() { return s => this.includes(s) } })
   Object.defineProperty(RegExp.prototype, "split", { get() { return s => s.split(this) } })
   const log = (...a) => (a.map(x => console.dir(x, {depth: null})), a[0])
+  const some = value => {
+    const f = (...a) => value(...a)
+    f.value = value
+    f.then = f => f(value)
+    f.or = () => value
+    return f
+  }
+  const none = () => none
+  none.then = () => none
+  none.or = v => v
 }).toString().slice("(() => {".length, -("})".length)) + ";\n"
 
 const compile = program => {
@@ -26,16 +38,16 @@ const compile = program => {
     const tokens = []
     let pos = 0
     let lineno = 1
-    for (const code of program.split(/([A-Za-z_][A-Za-z0-9_]*|-?0x[A-Fa-f0-9]+|-?[0-9]+(?:(?:\.[0-9]+)|(?:e[0-9]+))?|""".*?"""|"[^"]*?"|```.*?```|`[^`]*?`|[ \r\n\t]+|[()\[\]{};]|#[^\n]*)/)) {
+    for (const code of program.split(/([A-Za-z_][A-Za-z0-9_]*\??|-?0x[A-Fa-f0-9]+|-?[0-9]+(?:(?:\.[0-9]+)|(?:e[0-9]+))?|""".*?"""|"[^"]*?"|```.*?```|`[^`]*?`|[ \r\n\t]+|[()\[\]{};]|#[^\n]*)/)) {
       if (/^[ \r\n\t#;]/.test(code) || code === "") {
         lineno += code.split(/\n|;/).length - 1
       } else {
         const op = /^[+\-*/%|&<>=!]+$/.test(code)
-        const op2 = op && " \n\t".includes(program[pos + code.length])
-        const op1 = op && !op2
+        const op2 = op && (" \n\t".includes(program[pos + code.length]) || (pos > 0 && !(" \n\t".includes(program[pos - 1]))))
+        const op1 = (op || code === "...") && !op2
         const dot = code === "."
         const call = code === "(" && (pos > 0 && !("\n\t ".includes(program[pos-1])))
-        const index = code === "[" && (pos > 0 && !("\n\t ".includes(program[pos-1])))
+        const index = code === "[" && (pos > 0 && !("\n\t .".includes(program[pos-1])))
         tokens.push({code, lineno, op, op1, op2, dot, call, index})
       }
       pos += code.length
@@ -89,30 +101,32 @@ const compile = program => {
   const generate = root => {
     const genif = a => a.length === 1 ? a[0] : `if (${a[0]}) { ${a[1]} }${a.length == 2 ? "" : " else " + genif(a.slice(2))}`
     const geniif = a => a.length === 1 ? a[0] : `${a[0]} ? ${a[1]} : ${geniif(a.slice(2))}`
-    const genenum = a => a.map(gentag).join(";")
+    const genenum = a => a.map(gentag).join(";\n")
     const gentag = t => Array.isArray(t) ? `let ${t[0].code} = (__value) => ({__tag: "${t[0].code}", __value})` : `let ${t.code} = {__tag: "${t.code}"}`
     const genmatch = a => a.length === 0 ? "(() => { throw new Error(`No match for ${JSON.stringify(__target)}`) })()" :
       a.length === 1 ? gen(a[0]) :
       `__target.__tag === "${a[0].code}" ? ${gencase(a[1])} : ${genmatch(a.slice(2))}`
     const gencase = x => Array.isArray(x) && x[0].code === "fn" ? `${gen(x)}(__target.__value)` : gen(x)
     const genbody = x => Array.isArray(x) && "{ if while return".split(" ").includes(x[0].code) ? "{" + genreturn(x.slice(1).map(gen)) + "}" : gen(x)
-    const genreturn = a => a.slice(0, -1).join(";") + ";" + (a.at(-1).startsWith("return") ? a.at(-1) : "return " + a.at(-1))
+    const genreturn = a => a.slice(0, -1).join(";\n") + ";" + (a.at(-1).startsWith("return") ? a.at(-1) : "return " + a.at(-1))
+    const genarg = (s, t) => t.code.endsWith("?") ? `_${s}=none,${s}=(_${s} === none ? none : some(_${s}))` : s
     const gen = node => {
       if (Array.isArray(node)) {
         const head = node[0]
         const tail = node.slice(1)
-        return head.op2 ? gen(tail[0]) + head.code + gen(tail[1]) :
+        return head.op1 ? head.code + gen(tail[0]) :
+          head.op2 ? gen(tail[0]) + head.code + gen(tail[1]) :
           head.dot ? gen(tail[0]) + "." + tail[1].code :
           head.index ? gen(tail[0]) + ".at(" + gen(tail[1]) + ")" :
           head.code === "("      ? "(" + gen(tail[0]) + ")" :
           head.code === "["      ? "[" + tail.map(gen) + "]" :
-          head.code === "{"      ? tail.length === 1 ? gen(tail[0]) : tail.map(gen).join(";") :
-          head.code === "fn"     ? `((${tail.slice(0, -1).map(gen)}) => ${genbody(tail.at(-1))})` :
+          head.code === "{"      ? tail.length === 1 ? gen(tail[0]) : tail.map(gen).join(";\n") :
+          head.code === "fn"     ? `((${tail.slice(0, -1).map(t => genarg(gen(t), t))}) => ${genbody(tail.at(-1))})` :
           head.code === "iif"    ? geniif(tail.map(gen)) :
           head.code === "match"  ? `(__target => ${genmatch(tail.slice(1))})(${gen(tail[0])})` :
           head.code === "var"    ? `let ${tail[0][1].code} = ${gen(tail[0][2])}` :
           head.code === "let"    ? `const ${tail[0][1].code} = ${gen(tail[0][2])}` :
-          head.code === "def"    ? `const ${tail[0].code} = (${tail.slice(1, -1).map(gen).join(", ")}) => ${genbody(tail.at(-1))}` :
+          head.code === "def"    ? `const ${tail[0].code} = (${tail.slice(1, -1).map(t => genarg(gen(t), t)).join(", ")}) => ${genbody(tail.at(-1))}` :
           head.code === "enum"   ? `${genenum(tail[1].slice(1))}\nlet ${tail[0].code} = {${tail[1].slice(1).map(x => Array.isArray(x) ? x[0].code : x.code)}}` :
           head.code === "class"  ? `let ${tail[0].code} = ${(a => `(${a}) => ({${a}})`)(tail[1].slice(1).map(t => t[0].code))}` :
           head.code === "if"     ? genif(tail.map(gen)) :
@@ -135,7 +149,7 @@ const compile = program => {
 
   const tokens = tokenize(program)
   const trees = parse(tokens)
-  const js = trees.map(generate).join(";")
+  const js = trees.map(generate).join(";\n")
   return { tokens, trees, js }
 }
 
@@ -170,7 +184,6 @@ const runTest = eq => {
   test("fn(1)()", 1)
   test("fn(a a)(1)", 1)
   test("fn(a b a + b)(1 2)", 3)
-  test("fn(a b? a)(1)", 1)
 
   // Syntax sugars
   test("1 #a", 1)
@@ -179,6 +192,7 @@ const runTest = eq => {
   test("1\nfn(a a) 2", 2)
   test("!true", false)
   test("1 + 2", 3)
+  test("1+2", 3)
   test("fn(1)()", 1)
   test('"abc".size', 3)
   test('"abc".slice(1)', "bc")
@@ -202,11 +216,25 @@ const runTest = eq => {
   test("[1 2]", [1, 2])
   test("[1][0]", 1)
   test("[1 2][-1]", 2)
+  test("fn(a b? a) 1", 1)
+  test("fn(a b? b.or(a)) 1", 1)
+  test("fn(a b? b.or(a)) 1 2", 2)
+  test("fn(a? a?(1)) fn(a a)", 1)
+  test("fn(a? a?(1))()", () => none)
+  test("def f a b? a; f 1", 1)
+  test("def f a b? b.or(a); f 1", 1)
+  test("def f a b? b.or(a); f 1 2", 2)
+  test("def f a? a?(1); f fn(a a)", 1)
+  test("def f a? a?(1); f()", () => none)
+  test("fn(a a) ...[1]", 1)
+  test("fn(a b a + b) ...[1 2]", 3)
+  test("[...[1]]", [1])
+  test("[...[1 2]]", [1, 2])
 
   // Definitions
   test("let a = 1; a", 1)
   test("let f = fn(1); f()", 1)
-  test("let f = fn(a a); f(2)", 2)
+  test("let f = fn(a a); f 2", 2)
   test("def f 1; f()", 1)
   test("def f a a; f 1", 1)
   test("def f a b a + b; f 1 2", 3)
@@ -216,6 +244,9 @@ const runTest = eq => {
   test("enum ab { a; b int }; match b(2) a 1 b fn(n n)", 2)
   test("class ab { a int; b int }; ab(1 2).a", 1)
   test("class ab { a int; b int }; ab(1 2).b", 2)
+
+  // Assignment
+  test("var a = 0; a += 1", 1)
 
   // Branches
   test("throw 1", "error: 1")
@@ -253,6 +284,8 @@ const runTest = eq => {
   test('"a".has("a")', true)
   test('"".present', false)
   test('"a".present', true)
+  test('"".size', 0)
+  test('[].size', 0)
 
   // Others
   test("let x = true && !false; x", true)
@@ -262,10 +295,12 @@ const runTest = eq => {
 const testInterpriter = () => runTest((code, expected) => {
   const {tokens, trees, js} = compile(code)
   const actual = runJs(js, {console: {dir: () => null}})
-  const str = o => o.constructor.name === "RegExp" ? o.toString() : typeof o === "object" ? JSON.stringify(o) : o
+  const str = o => typeof o === "function" || o.constructor.name === "RegExp" ? o.toString() :
+    typeof o === "object" ? JSON.stringify(o) :
+    o
   return (str(expected) === str(actual)) || `Test failed
-Expected: ${expected}
-  Actual: ${actual}
+Expected: ${expected} :: ${typeof expected}
+  Actual: ${actual} :: ${typeof actual}
     Code: ${code}
       JS: ${js}
    Trees: ${trees.map(showNode)}
