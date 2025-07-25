23afba2fa55ccb810cb8777e3ce48f02f1fa5403
hiroshi <hiroshi@wovn.io>
2025-07-25 14:57:49 +0900

    parse

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -5,17 +5,22 @@ const log = x => { console.dir(x, {depth: null}); return x }
 
 function tokenize(program) {
   const reg = /(r\/(?:[^\/\\]|\\.)*?\/[A-Za-z]*|[0-9]+|(?:\.\.\.)?[A-Za-z0-9_]+|[\.()[\]{}]|"(?:[^"\\]|\\.)*"|`(?:[^`\\]|\\.)*`|[+\-*\/%<>!=^|&]+|#[^\n]*| +|\n+)/g
+  const tokens = []
   let offset = 0
   let lineno = 1
-  return program.match(reg).map(code => ({
-    code,
-    offset: offset += code.length,
-    lineno: code.startsWith("\n") ? lineno += code.split("\n").length - 1 : lineno
-  })).filter(t => t.code.trim().replace(/^#.*/, ""))
+  for (const code of program.match(reg)) {
+    if (code.match(/^[\n #]/)) {
+      lineno += code.split("\n").length - 1
+    } else {
+      tokens.push({code, offset, lineno})
+    }
+    offset += code.length
+  }
+  return tokens
 }
 
 function testTokenize() {
-  const eq = (expected, program) => assert.strictEqual(tokenize(program).map(t => t.code).join(" "), expected)
+  const eq = (expected, program) => assert.strictEqual(tokenize(program).map(t => t.code).join(" "), expected, program)
   eq("0 a b1 ( ) [ ] { } \"A\" `B` + ** %=", "0 a b1 ()[]{} \"A\" `B` + ** %= #comment")
   eq('"a\\"\\nb"', `"a\\\"\\nb"`)
   eq("r/a/", "r/a/")
@@ -25,13 +30,14 @@ function testTokenize() {
 }
 
 function parse(tokens) {
-  const then = (a, ...fs) => fs.length === 1 ? fs[0](...a) : then(fs[0](...a), ...fs.slice(1))
+  const then = (a, f) => f(...a)
   const fail = s => { throw new Error(s) }
   const isOp = i => /^[+\-*\/%<>!=^|&]/.test(tokens[i]?.code)
-  const isOp1 = i => isOp(i) && tokens[i].offset === tokens[i+1]?.offset - tokens[i+1]?.code?.length
+  const isOp1 = i => isOp(i) && closeRight(i) && !closeLeft(i)
   const isOp2 = i => isOp(i) && !isOp1(i)
   const fake = i => ({...tokens[i], code: "__" + tokens[i].code})
-  const closeLeft = i => tokens[i-1].offset === tokens[i].offset - tokens[i].code.length
+  const closeLeft = i => i >= 1 && tokens[i-1].offset + tokens[i-1].code.length === tokens[i].offset
+  const closeRight = i => (i+1) < tokens.length && tokens[i].offset + tokens[i].code.length === tokens[i+1].offset
   const until = (i, end, a) => untilBy(i, end, a, unit)
   const untilBy = (i, end, a, f) =>
     i >= tokens.length ? fail(`Not closed '${end}'`) :
@@ -44,11 +50,11 @@ function parse(tokens) {
     isOp2(i)                                ? then(unit(i+1), (j, next) => link(j, [tokens[i], node, next])) :
     [i, node]
   const unit = i =>
-    isOp1(i)                ? then(unit(i+1), (j, node) => [j, [tokens[i], node]], link) :
+    isOp1(i)                             ? then(unit(i+1), (j, node) => [j, [tokens[i], node]]) :
     tokens[i]?.code === "(" && isOp(i+1) ? then(untilBy(i+1, ")", [tokens[i]], j => [j+1, tokens[j]]), link) :
-    tokens[i]?.code === "(" ? then(until(i+1, ")", [tokens[i]]), link) :
-    tokens[i]?.code === "[" ? then(until(i+1, "]", [tokens[i]]), link) :
-    tokens[i]?.code === "{" ? then(lines(i+1, [tokens[i]]), (j, a) => tokens[j].code === "}" ? [j+1, a] : fail(`${tokens[i]} is not closed until ${tokens[j]}`)) :
+    tokens[i]?.code === "("              ? then(until(i+1, ")", [tokens[i]]), link) :
+    tokens[i]?.code === "["              ? then(until(i+1, "]", [tokens[i]]), link) :
+    tokens[i]?.code === "{"              ? then(lines(i+1, [tokens[i]]), (j, a) => tokens[j].code === "}" ? [j+1, a] : fail(`${tokens[i]} is not closed until ${tokens[j]}`)) :
     link(i+1, tokens[i])
   const line = (i, lineno, acc) =>
     tokens[i]?.lineno === lineno && tokens[i]?.code !== "}" ? then(unit(i), (j, node) => line(j, lineno, acc.concat([node]))) :
@@ -56,13 +62,12 @@ function parse(tokens) {
   const lines = (i, acc) =>
     tokens[i] ? then(line(i, tokens[i].lineno, []), (j, a) => a.length === 0 ? [j, acc] : lines(j, acc.concat([a]))) :
     [i, acc]
-  const result = lines(0, [])
-  return result[1]
+  return lines(0, [])[1]
 }
 
 function testParse() {
   const show = x => Array.isArray(x) ? "(" + (x[0]?.code === "(" ? show(x[1]) : x.map(show).join(" ")) + ")" : x.code
-  const eq = (expected, program) => assert.strictEqual(parse(tokenize(program)).map(show).join("\n"), expected)
+  const eq = (expected, program) => assert.strictEqual(parse(tokenize(program)).map(show).join("\n"), expected, program)
   eq("a", "a")
   eq("(a)", "(a)")
   eq("(a b)", "a b")
@@ -71,10 +76,12 @@ function testParse() {
   eq("(__[ a b)", "a[b]")
   eq("(= (__[ a b) c)", "a[b] = c")
   eq("(+ a)", "+a")
+  eq("(+ a b)", "a+b")
   eq("(+ a b)", "a + b")
   eq("(+ a (- b))", "a + -b")
   eq("(+ a (* b c))", "a + b * c")
   eq("(* ((+ a b)) c)", "(a + b) * c")
+  eq("(&& (a b) (! (c d)))", "a(b) && !c(d)")
   eq("(. a b)", "a.b")
   eq("((. a b) c)", "a.b(c)")
   eq("((. ((. a b) c) d) e)", "a.b(c).d(e)")
@@ -94,6 +101,7 @@ function genjs(nodes) {
   const gencode = code => code.startsWith("r/") ? code.slice(1) : code
   const genmatch = ([x, y, ...z]) => `__m === ${x} ? ${y} : ${z.length === 0 ? '__fail(`Unmatch ${__m}`)' : z.length === 1 ? z[0] : genmatch(z)}`
   const genclass = fields => `(${fields}) => ({${fields}})`
+  const geniif = a => a.length === 1 ? a[0] : `${a[0]} ? ${a[1]} : ${geniif(a.slice(2))}`
   const gencall = (head, tail) =>
     isOp(head.code) ? (tail.length === 2 ? gen(tail[0]) + genop2(head.code) + gen(tail[1]) : genop1(head.code) + gen(tail[0])) :
     head.code === "test"  ? `__tests.push(${tail[0].code} => ${gen(tail.at(-1))})` :
@@ -107,6 +115,7 @@ function genjs(nodes) {
     head.code === "else" && tail[0].code === "if"  ? `else if (${gen(tail[1])}) ${gen(tail[2])}` :
     head.code === "else"  ? "else " + gen(tail[0]) :
     head.code === "match" ? `(__m => ${genmatch(tail.slice(1).map(gen))})(${gen(tail[0])})` :
+    head.code === "iif"   ? geniif(tail.map(gen)) :
     head.code === "fn"    ? `(${tail.slice(0, -1).map(gen)}) => ${gen(tail.at(-1))}` :
     head.code === "__["   ? `${gen(tail[0])}[${gen(tail[1])}]` :
     head.code === "("     ? `(${gen(tail[0])})` :
@@ -119,7 +128,7 @@ function genjs(nodes) {
 }
 
 function testGenjs() {
-  const eq = (expected, program) => assert.strictEqual(genjs(parse(tokenize(program))), expected)
+  const eq = (expected, program) => assert.strictEqual(genjs(parse(tokenize(program))), expected, program)
   eq("a", "a")
   eq("/a/", "r/a/")
   eq('"a\\\\"', '"a\\\\"')
@@ -152,15 +161,20 @@ function testGenjs() {
   eq("for (const x of xs) {\n\n}", "each x xs {}")
   eq("const a = (b) => ({b})", "class a { b c }")
   eq("const a = (b,e) => ({b,e})", "class a { b c\ne f }")
+  eq("a ? b : c", "iif a b c")
 }
 
 const runtime = (() => {"use strict"
 const io = {
-  log: (...a) => console.error(...a),
+  log: x => { console.error(JSON.stringify(x, null, 2)); return x }
 }
+const fail = s => { throw new Error(s) }
+const tuple = (...a) => a
 Array.prototype.fmap = function(f) { return this.flatMap(f) }
+Array.prototype.tmap = function(f) { return f(...this) }
+Object.defineProperty(Array.prototype, 'size', { get() { return this.length } })
 String.prototype.starts = function(s) { return this.startsWith(s) }
-String.prototype.count = function(s) { return this.split(s).length }
+String.prototype.count = function(s) { return this.split(s).length - 1 }
 Object.defineProperty(String.prototype, 'size', { get() { return this.length } })
 const __assert = require("node:assert")
 const __fail = (...a) => { throw new Error(`${JSON.stringify(a)}`) }
